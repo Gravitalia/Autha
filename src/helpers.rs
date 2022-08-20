534a15a3e89841752952aae0c3cfb5b4ac61f20e
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-08-21 00:53:44 +0200

    Update ChaCha20 💎

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,9 +1,9 @@
 use argon2::{self, Config, ThreadMode, Variant, Version};
-use regex::Regex;
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
-    ChaCha20Poly1305, Nonce
+    ChaCha20Poly1305
 };
+use generic_array::GenericArray;
 
 pub fn random_string() -> String {
     let chars: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".chars().collect();
@@ -28,35 +28,37 @@ pub fn hash(data: &[u8]) -> String {
             variant: Variant::Argon2id,
             version: Version::Version13,
             mem_cost: 32768,
-            time_cost: 7,
+            time_cost: dotenv::var("ROUND").expect("Missing env `ROUND`").parse::<u32>().unwrap(),
             lanes: 8,
             thread_mode: ThreadMode::Parallel,
             secret: dotenv::var("KEY").expect("Missing env `KEY`").as_bytes(),
             ad: &[],
-            hash_length: 64
+            hash_length: dotenv::var("HASH_LENGTH").expect("Missing env `HASH_LENGTH`").parse::<u32>().unwrap()
         }
     ).unwrap()
 }
 
-pub fn encrypt(data: String) -> String {
-    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+pub fn encrypt(data: &[u8]) -> String {
+    match hex::decode(dotenv::var("CHA_KEY").expect("Missing env `CHA_KEY`")) {
+        Ok(v) => {
+            let bytes: generic_array::GenericArray<u8, generic_array::typenum::UInt<generic_array::typenum::UInt<generic_array::typenum::UInt<generic_array::typenum::UInt<generic_array::typenum::UInt<generic_array::typenum::UInt<generic_array::typenum::UTerm, generic_array::typenum::B1>, generic_array::typenum::B0>, generic_array::typenum::B0>, generic_array::typenum::B0>, generic_array::typenum::B0>, generic_array::typenum::B0>> = GenericArray::clone_from_slice(&v);
 
-    match hex::decode(dotenv::var("KEY").expect("Missing env `KEY`")) {
-        Ok(d) => println!("{:?}", d),
-        Err(_) => println!("ok"),
+            let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
+            match ChaCha20Poly1305::new(&bytes).encrypt(&nonce, data) {
+                Ok(v) => format!("{}//{}", hex::encode(nonce), hex::encode(v)),
+                Err(_) => "Error".to_string(),
+            }
+        },
+        Err(_) => "Error".to_string(),
     }
-
-    println!("{}", hex::encode(key));
-    let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
-    match ChaCha20Poly1305::new(&key).encrypt(&nonce, data.as_bytes().as_ref()) {
-        Ok(v) => println!("{}//{}", hex::encode(nonce), hex::encode(v)),
-        Err(e) => println!("error parsing header: {e:?}"),
-    }
-
-    "test".to_string()
 }
 
 #[test]
 fn test_hash() {
-    assert!(Regex::new(r"[$]argon2(i)?(d)?[$]v=[0-9]{1,2}[$]m=[0-9]+,t=[0-9]{1,},p=[0-9]{1,}[$].*").unwrap().is_match(&hash("password".as_bytes())));
+    assert!(regex::Regex::new(r"[$]argon2(i)?(d)?[$]v=[0-9]{1,2}[$]m=[0-9]+,t=[0-9]{1,},p=[0-9]{1,}[$].*").unwrap().is_match(&hash("password".as_bytes())));
+}
+
+#[test]
+fn test_encrypt() {
+    assert!(regex::Regex::new(r"[0-9a-fA-F]{24}[/]{2}[0-9a-fA-F]+").unwrap().is_match(&encrypt("I'm feeling lucky".as_bytes())));
 }
