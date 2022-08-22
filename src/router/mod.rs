9f79e6ac8307443098e374fb8e5c15901b1b2026
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-08-22 13:58:29 +0200

    Adding phone & birth 🎂

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -29,17 +29,29 @@ pub fn create(body: model::Create, _finger: String) -> WithStatus<Json> {
     if body.username.len() >= 16 {
 		return err("Invalid username".to_string());
 	}
-
+    // Phone verification
+    let mut phone: String = "".to_string();
     if body.phone.is_some() {
-        if !Regex::new(r"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$").unwrap().is_match(&body.phone.unwrap()) {
+        if !Regex::new(r"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$").unwrap().is_match(body.phone.as_ref().unwrap()) {
             return err("Invalid phone".to_string());
+        } else {
+            phone = super::helpers::encrypt(body.phone.unwrap().as_bytes())
+        }
+    }
+    // Birthdate verification
+    let mut birth: String = "".to_string();
+    if body.birthdate.is_some() {
+        if !Regex::new(r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$").unwrap().is_match(body.birthdate.as_ref().unwrap()) {
+            return err("Invalid birthdate".to_string());
+        } else {
+            birth = super::helpers::encrypt(body.birthdate.unwrap().as_bytes())
         }
     }
 
     warp::reply::with_status(warp::reply::json(
         &model::CreateResponse{
-            token: "token".to_string(),
-            vanity: "vanity".to_string()
+            token: phone,
+            vanity: super::helpers::hash("test".as_bytes())
         }
     ),
     warp::http::StatusCode::CREATED)
