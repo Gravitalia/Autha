23137dae29457bb104801d2c5e2cf4d848a803a9
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-08-22 19:01:26 +0200

    Updates create route ☄️

---
+++ main.rs
@@ -0,0 +1,20 @@
+use warp::Filter;
+mod router;
+mod helpers;
+mod database;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let routes = warp::path!("create").and(warp::post()).and(warp::body::json()).and(warp::header("sec")).map(router::create::create);
+
+    database::cassandra::test().await;
+
+    warp::serve(routes)
+    .run((
+        [127, 0, 0, 1],
+        dotenv::var("PORT").expect("Missing env `PORT`").parse::<u16>().unwrap(),
+    ))
+    .await;
+}
