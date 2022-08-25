1d2af96283aac961ef2798d7ff006b327f2d226f
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-08-25 19:17:35 +0200

    Async route 🔂

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ mod database;
 async fn main() {
     dotenv::dotenv().ok();
 
-    let routes = warp::path!("create").and(warp::post()).and(warp::body::json()).and(warp::header("sec")).map(router::create::create);
+    let routes = warp::path("create").and(warp::post()).and(warp::body::json()).and(warp::header("sec")).and_then(|body: router::model::Create, finger: String| async {
+        if true {
+            Ok(router::create::create(body, finger).await)
+        } else {
+            Err(warp::reject::not_found())
+        }
+    });
 
     database::cassandra::init().await;
     database::cassandra::tables().await;
