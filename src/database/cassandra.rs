aac01c298e9683ef9af561a63296420ffff5d6c4
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-09-25 17:05:57 +0200

    Update rejection 🎨

--- src/database/cassandra.rs
+++ src/database/cassandra.rs
@@ -17,6 +17,7 @@ pub async fn tables() {
     SESSION.get().unwrap().query("CREATE TABLE IF NOT EXISTS accounts.users ( vanity text, email text, username text, avatar text, banner text, bio text, verified boolean, flags int, phone text, password text, birthdate text, deleted boolean, mfa_code text, oauth list<text>, PRIMARY KEY (vanity, email) ) WITH compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.ZstdCompressor'} AND gc_grace_seconds = 864000;").await.expect("accounts.users create error");
     SESSION.get().unwrap().query("CREATE TABLE IF NOT EXISTS accounts.security ( id text, user_id text, fingerprint text, ip text, country text, revoked boolean, type int, created_at timestamp, PRIMARY KEY (id) ) WITH caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'} AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.ZstdCompressor'} AND gc_grace_seconds = 172800 AND default_time_to_live = 15770000;").await.expect("accounts.security create error");
     SESSION.get().unwrap().query("CREATE TABLE IF NOT EXISTS accounts.bots ( id text, user_id text, client_secret text, ip text, username text, avatar text, bio text, flags int, deleted boolean, PRIMARY KEY (id, username) ) WITH compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.ZstdCompressor'} AND gc_grace_seconds = 864000;").await.expect("accounts.bots create error");
+    SESSION.get().unwrap().query("CREATE INDEX IF NOT EXISTS ON accounts.users (email);").await.expect("second index (email) create error");
 }
 
 pub async fn create_user(vanity: String, email: String, username: String, password: String, phone: Option<String>, birthdate: Option<String>) {
@@ -38,5 +39,5 @@ pub async fn create_security(vanity: String, code: u8, fingerprint: String, ip: 
 }
 
 pub async fn query(query: &'static str, params: Vec<String>) -> cdrs_tokio::frame::Frame {
-    SESSION.get().unwrap().query_with_values(query, params).await.expect("Keyspace create error")
+    SESSION.get().unwrap().query_with_values(query, params).await.expect("Query error")
 }
