e6a4d1972c97e09bfe54817c3de8ab5fe3f8930b
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-10-08 11:43:31 +0200

    First login route ✨

--- src/router/login.rs
+++ src/router/login.rs
@@ -3,5 +3,10 @@ use warp::reply::{WithStatus, Json};
 use sha256::digest;
 
 pub async fn login(body: super::model::Create, finger: String) -> WithStatus<Json> {
-
+    warp::reply::with_status(warp::reply::json(
+        &super::model::CreateResponse{
+            token: "ok".to_string()
+        }
+    ),
+    warp::http::StatusCode::OK)
 }
