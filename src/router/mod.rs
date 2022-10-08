b37378f9edd4ce9ce474bbca30e2493e400aa5bd
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-10-08 11:43:03 +0200

    Login route ✨

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -2,6 +2,7 @@ pub mod model;
 use warp::reply::{WithStatus, Json};
 pub mod create;
 pub mod users;
+pub mod login;
 
 fn err(message: String) -> WithStatus<Json> {
     warp::reply::with_status(warp::reply::json(
