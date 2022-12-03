0082eb249da07cc0330b5a2c69d2048a871f19d2
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-04 00:05:15 +0100

    Add suspend function ✨

--- src/database/cassandra.rs
+++ src/database/cassandra.rs
@@ -31,3 +31,10 @@ pub async fn create_security(vanity: String, _code: u8, fingerprint: String, ip:
 pub async fn query(query: &'static str, params: Vec<String>) -> scylla::QueryResult {
     SESSION.get().unwrap().query(query, params).await.expect("Query error")
 }
+
+pub async fn suspend(vanity: String) -> scylla::QueryResult {
+    // Send request to mod center
+    SESSION.get().unwrap().query("UPDATE accounts.users SET deleted = ?, verified = ?, birthdate = ?, mfa_code = ? WHERE vanity = ?", (true, false, "", "", vanity.clone())).await.expect("Query error");
+    SESSION.get().unwrap().query("UPDATE accounts.security SET revoked = ?, fingerprint = ? WHERE user_id = ? ALLOW FILTERING", (true, "", vanity.clone())).await.expect("Query error");
+    SESSION.get().unwrap().query("UPDATE accounts.bots SET deleted = ?, ip = ? WHERE user_id = ? ALLOW FILTERING", (true, "", vanity)).await.expect("Query error")
+}
