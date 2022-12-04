31c113918e1a18654cf111a8972c7faa84dc5f23
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-04 15:43:11 +0100

    Add and update functions ✨

--- src/database/cassandra.rs
+++ src/database/cassandra.rs
@@ -32,9 +32,15 @@ pub async fn query(query: &'static str, params: Vec<String>) -> scylla::QueryRes
     SESSION.get().unwrap().query(query, params).await.expect("Query error")
 }
 
+pub async fn update_user(params: (String, Option<String>, Option<String>, Option<String>, Option<String>, String, String)) -> scylla::QueryResult {
+    SESSION.get().unwrap().query("UPDATE accounts.users SET username = ?, avatar = ?, bio = ?, birthdate = ?, phone = ?, email = ? WHERE vanity = ?", params).await.expect("Query error")
+}
+
 pub async fn suspend(vanity: String) -> scylla::QueryResult {
     // Send request to mod center
-    SESSION.get().unwrap().query("UPDATE accounts.users SET deleted = ?, verified = ?, birthdate = ?, mfa_code = ? WHERE vanity = ?", (true, false, "", "", vanity.clone())).await.expect("Query error");
-    SESSION.get().unwrap().query("UPDATE accounts.security SET revoked = ?, fingerprint = ? WHERE user_id = ? ALLOW FILTERING", (true, "", vanity.clone())).await.expect("Query error");
-    SESSION.get().unwrap().query("UPDATE accounts.bots SET deleted = ?, ip = ? WHERE user_id = ? ALLOW FILTERING", (true, "", vanity)).await.expect("Query error")
+    SESSION.get().unwrap().query("UPDATE accounts.users SET deleted = ?, verified = ? WHERE vanity = ?", (true, false, vanity.clone())).await.expect("Query error")
+
+    // Get partition key
+    //SESSION.get().unwrap().query("UPDATE accounts.bots SET deleted = ?, ip = ? WHERE user_id = ?", (true, "", vanity)).await.expect("Query error");
+    //SESSION.get().unwrap().query("UPDATE accounts.security SET revoked = ?, fingerprint = ? WHERE user_id = ?", (true, "", vanity.clone())).await.expect("Query error")
 }
