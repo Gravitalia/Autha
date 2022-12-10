e8234c9fcb95201f9b778375e38bec2d91aa0a7a
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-10 17:40:46 +0100

    Add documentation 📝

--- src/database/cassandra.rs
+++ src/database/cassandra.rs
@@ -5,10 +5,12 @@ use uuid::Uuid;
 use once_cell::sync::OnceCell;
 static SESSION: OnceCell<scylla::Session> = OnceCell::new();
 
+/// Start Cassandra session
 pub async fn init() {
     let _session = SESSION.set(SessionBuilder::new().known_node("127.0.0.1:9042").build().await.unwrap());
 }
 
+/// Create tables in your Cassandra keyspace
 pub async fn tables() {
     SESSION.get().unwrap().query("CREATE KEYSPACE IF NOT EXISTS accounts WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };", &[]).await.expect("Keyspace create error");
     SESSION.get().unwrap().query("CREATE TABLE IF NOT EXISTS accounts.users ( vanity text, email text, username text, avatar text, banner text, bio text, verified boolean, flags int, phone text, password text, birthdate text, deleted boolean, mfa_code text, oauth list<text>, PRIMARY KEY (vanity) ) WITH compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.ZstdCompressor'} AND gc_grace_seconds = 864000;", &[]).await.expect("accounts.users create error");
@@ -17,10 +19,15 @@ pub async fn tables() {
     SESSION.get().unwrap().query("CREATE INDEX IF NOT EXISTS ON accounts.users (email);", &[]).await.expect("second index (email) create error");
 }
 
+/// Create a user for account delegation
+/// ```rust
+/// create_user(&"realhinome".to_string(), "email@mail.com".to_string(), "Hinome".to_string(), "SuperPassword_123456".to_string(), None, None)
+/// ```
 pub async fn create_user(vanity: &String, email: String, username: String, password: String, phone: Option<String>, birthdate: Option<String>) {
     SESSION.get().unwrap().query("INSERT INTO accounts.users (vanity, email, username, password, phone, birthdate, flags, deleted, verified) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (vanity, email, username, password, phone, birthdate, 0, false, false)).await.expect("Failed to create user");
 }
 
+/// Create a security token, used for email, phone verification and JWT centralization security
 pub async fn create_security(vanity: String, _code: u8, fingerprint: String, ip: Option<String>, country: Option<String>) -> uuid::Uuid {
     let id: uuid::Uuid = Uuid::new_v4();
 
@@ -32,15 +39,18 @@ pub async fn query(query: &'static str, params: Vec<String>) -> scylla::QueryRes
     SESSION.get().unwrap().query(query, params).await.expect("Query error")
 }
 
+/// Update user data (public or authorization)
 #[allow(clippy::type_complexity)]
 pub async fn update_user(params: (String, Option<String>, Option<String>, Option<String>, Option<String>, String, String)) -> scylla::QueryResult {
     SESSION.get().unwrap().query("UPDATE accounts.users SET username = ?, avatar = ?, bio = ?, birthdate = ?, phone = ?, email = ? WHERE vanity = ?", params).await.expect("Query error")
 }
 
+/// Update the user password, need to be hashed first
 pub async fn update_password(new_password: String, vanity: String) -> scylla::QueryResult {
     SESSION.get().unwrap().query("UPDATE accounts.users SET password = ? WHERE vanity = ?", (new_password, vanity)).await.expect("Query error")
 }
 
+/// Ban a user account, this will make a request in Central Moderation (Signaly)
 pub async fn suspend(vanity: String) -> scylla::QueryResult {
     // Send request to mod center
     SESSION.get().unwrap().query("UPDATE accounts.users SET deleted = ?, verified = ? WHERE vanity = ?", (true, false, vanity.clone())).await.expect("Query error")
