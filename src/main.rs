3c5971fb3eb45a8c8395ca0ccc99d950383d3bdb
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-01-03 21:12:52 +0100

    Add serve ✨

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let routes = warp::path("").map(|| "test");
+
+    warp::serve(warp::any().and(warp::options()).map(|| "OK").or(warp::head().map(|| "OK"))).or(routes)
+    .run((
+        [127, 0, 0, 1],
+        dotenv::var("PORT").expect("Missing env `PORT`").parse::<u16>().unwrap(),
+    ))
+    .await;
 }
