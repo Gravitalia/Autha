7dcaa668b0471a6c2998549e02da0d1ea5f98a5d
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-01-04 21:05:54 +0100

    Wrong path 🚑️

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -4,7 +4,7 @@ use warp::reply::{WithStatus, Json};
 /// Create message error easier
 fn err(message: String) -> WithStatus<Json> {
     warp::reply::with_status(warp::reply::json(
-        &model::Error{
+        &crate::model::Error::Error{
             error: true,
             message,
         }
