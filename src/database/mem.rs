aec599377afe0eedae2a902d72ec4159ab69c9f3
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-01-14 16:50:30 +0100

    Add global variable 🎨

--- src/database/mem.rs
+++ src/database/mem.rs
@@ -1,29 +1,33 @@
 use memcache::{Client, MemcacheError};
+use once_cell::sync::OnceCell;
+static SESSION: OnceCell<Client> = OnceCell::new();
 
 pub enum SetValue {
     Characters(String),
     Number(u16),
 }
 
-pub fn init() -> Result<Client, MemcacheError> {
-    memcache::connect("memcache://127.0.0.1:11211?timeout=2&tcp_nodelay=true")
+pub fn init() -> Result<(), MemcacheError> {
+    let _ = SESSION.set(memcache::connect("memcache://127.0.0.1:11211?timeout=2&tcp_nodelay=true")?);
+
+    Ok(())
 }
 
-pub fn set(session: Client, key: String, value: SetValue) -> Result<(), MemcacheError> {
+pub fn set(key: String, value: SetValue) -> Result<(), MemcacheError> {
     match value {
         SetValue::Characters(data) => {
-            session.set(&key, data, 300)?;
+            SESSION.get().unwrap().set(&key, data, 300)?;
         },
         SetValue::Number(data) => {
-            session.set(&key, data, 300)?;
+            SESSION.get().unwrap().set(&key, data, 300)?;
         }
     };
 
     Ok(())
 }
 
-pub fn get(session: Client, key: String) -> Result<Option<String>, MemcacheError> {
-    let value: Option<String> = session.get(&key)?;
+pub fn get(key: String) -> Result<Option<String>, MemcacheError> {
+    let value: Option<String> = SESSION.get().unwrap().get(&key)?;
 
     Ok(value)
 }
