241a7474090b2c30b969dfd87905cce8306094f6
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-01-14 21:34:29 +0100

    Add function ✨

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -11,3 +11,14 @@ fn err(message: String) -> WithStatus<Json> {
     ),
     warp::http::StatusCode::BAD_REQUEST)
 }
+
+/// Return an error with rate limit informations
+fn rate() -> WithStatus<Json> {
+    warp::reply::with_status(warp::reply::json(
+        &crate::model::error::Error{
+            error: true,
+            message: "Too many requests".to_string(),
+        }
+    ),
+    warp::http::StatusCode::TOO_MANY_REQUESTS)
+}
