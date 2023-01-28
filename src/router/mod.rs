27023a47b018de20dc6df0ce84d71135f3671f9f
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-01-28 16:02:36 +0100

    Add new route ✨

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,25 +1,3 @@
-pub mod create;
-pub mod login;
-use warp::reply::{WithStatus, Json};
-
-/// Create message error easier
-fn err(message: String) -> WithStatus<Json> {
-    warp::reply::with_status(warp::reply::json(
-        &crate::model::error::Error{
-            error: true,
-            message,
-        }
-    ),
-    warp::http::StatusCode::BAD_REQUEST)
-}
-
-/// Return an error with rate limit informations
-fn rate() -> WithStatus<Json> {
-    warp::reply::with_status(warp::reply::json(
-        &crate::model::error::Error{
-            error: true,
-            message: "Too many requests".to_string(),
-        }
-    ),
-    warp::http::StatusCode::TOO_MANY_REQUESTS)
-}
+pub mod error;
+pub mod body;
+pub mod user;
