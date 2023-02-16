92bb441e2e6ff00cf6b1163fe6ad22b22f262c37
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-02-16 21:42:40 +0100

    Better code 🎨

--- src/helpers/jwt.rs
+++ src/helpers/jwt.rs
@@ -5,39 +5,33 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: String,
-    exp: u128,
+    exp: u64,
     iss: String,
-    iat: u128
+    iat: u64
 }
 
 /// Create a JWT token
 pub fn create_jwt(user_id: String) -> String {
-    match EncodingKey::from_rsa_pem(dotenv::var("RSA_PRIVATE_KEY").expect("Missing env `RSA_PRIVATE_KEY`").as_bytes()) {
-        Ok(d) => {
-            encode(&Header::new(Algorithm::RS256), &Claims {
-                sub: user_id.to_lowercase(),
-                exp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()+5259600000,
-                iss: "https://oauth.gravitalia.com".to_string(),
-                iat: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
-            }, &d).unwrap()
-        },
-        Err(_) => "Error".to_string(),
-    }
+    let private_key = EncodingKey::from_rsa_pem(dotenv::var("RSA_PRIVATE_KEY").expect("Missing env `RSA_PRIVATE_KEY`").as_bytes())
+        .expect("Failed to load private key");
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("SystemTime before UNIX EPOCH!");
+
+    encode(&Header::new(Algorithm::RS256), &Claims {
+        sub: user_id.to_lowercase(),
+        exp: now.as_secs() + 5259600,
+        iss: "https://oauth.gravitalia.com".to_string(),
+        iat: now.as_secs()
+    }, &private_key).unwrap()
 }
 
 /// Decode a JWT token and check if it is valid
 pub fn get_jwt(token: String) -> Result<TokenData<Claims>, String> {
-    match DecodingKey::from_rsa_pem(dotenv::var("RSA_PUBLIC_KEY").expect("Missing env `RSA_PUBLIC_KEY`").as_bytes()) {
-        Ok(d) => {
-            match decode::<Claims>(&token, &d, &Validation::new(Algorithm::RS256)) {
-                Ok(token_data) => {
-                    Ok(token_data)
-                },
-                Err(err) => Err(err.to_string()),
-            }
-        },
-        Err(_) => Err("Error".to_string()),
-    }
+    let public_key = DecodingKey::from_rsa_pem(dotenv::var("RSA_PUBLIC_KEY").expect("Missing env `RSA_PUBLIC_KEY`").as_bytes())
+        .expect("Failed to load public key");
+    decode::<Claims>(&token, &public_key, &Validation::new(Algorithm::RS256))
+        .map_err(|err| err.to_string())
 }
 
 #[cfg(test)]
