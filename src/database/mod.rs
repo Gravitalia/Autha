a674c91b7ac6d2c0bb169a5e744a8dbe485994bc
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-03-24 19:52:44 +0100

    Get bots ✨

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -8,7 +8,7 @@ pub fn get_user(vanity: String) -> Result<crate::model::user::User, String> {
             Ok(serde_json::from_str(&data[..]).unwrap())
         },
         None => {
-            let cassandra = match cassandra::query("SELECT username, avatar, bio, verified, deleted, flags FROM accounts.users WHERE vanity = ?", vec![vanity.clone()]) {
+            let mut cassandra = match cassandra::query("SELECT username, avatar, bio, deleted, flags, verified FROM accounts.users WHERE vanity = ?", vec![vanity.clone()]) {
                 Ok(x) => x.get_body().unwrap().as_cols().unwrap().rows_content.clone(),
                 Err(_) => {
                     return Err("Cassandra error".to_string());
@@ -16,14 +16,23 @@ pub fn get_user(vanity: String) -> Result<crate::model::user::User, String> {
             };
 
             if cassandra.is_empty() {
+                cassandra = match cassandra::query("SELECT username, avatar, bio, deleted, flags FROM accounts.bots WHERE id = ?", vec![vanity.clone()]) {
+                    Ok(x) => x.get_body().unwrap().as_cols().unwrap().rows_content.clone(),
+                    Err(_) => {
+                        return Err("Cassandra error".to_string());
+                    }
+                };
+            }
+
+            if cassandra.is_empty() {
                 Ok(crate::model::user::User {
                     username: "".to_string(),
                     vanity:  "".to_string(),
                     avatar: None,
                     bio: None,
-                    verified: false,
                     deleted: false,
                     flags: 0,
+                    verified: false,
                 })
             } else {
                 Ok(crate::model::user::User {
@@ -31,9 +40,9 @@ pub fn get_user(vanity: String) -> Result<crate::model::user::User, String> {
                     vanity,
                     avatar: if cassandra[0][1].clone().into_plain().is_none() { None } else { let res = std::str::from_utf8(&cassandra[0][1].clone().into_plain().unwrap()[..]).unwrap_or("").to_string(); if res.is_empty() { None } else { Some(res) } },
                     bio: if cassandra[0][2].clone().into_plain().is_none() { None } else { let res = std::str::from_utf8(&cassandra[0][2].clone().into_plain().unwrap()[..]).unwrap_or("").to_string(); if res.is_empty() { None } else { Some(res) } },
-                    verified: cassandra[0][3].clone().into_plain().unwrap()[..] != [0],
-                    deleted: cassandra[0][4].clone().into_plain().unwrap()[..] != [0],
-                    flags: u32::from_be_bytes((&cassandra[0][5].clone().into_plain().unwrap()[..])[..4].try_into().unwrap()),
+                    deleted: cassandra[0][3].clone().into_plain().unwrap()[..] != [0],
+                    flags: u32::from_be_bytes((&cassandra[0][4].clone().into_plain().unwrap()[..])[..4].try_into().unwrap()),
+                    verified: false,
                 })
             }
         }
