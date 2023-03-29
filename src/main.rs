d79b714ac3e8baf28d2f1ed43ec20021a229f22c
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-03-29 11:30:37 +0200

    Use new path 🐛

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ async fn main() {
         }
     })
     .or(warp::path("login").and(warp::post()).and(warp::body::json()).and(warp::header("cf-turnstile-token")).and(warp::addr::remote()).and_then(|body: model::body::Login, cf_token: String, ip: Option<SocketAddr>| async move {
-        match router::login::login(body, ip.unwrap_or_else(|| SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)).ip(), cf_token).await {
+        match router::login::main::login(body, ip.unwrap_or_else(|| SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)).ip(), cf_token).await {
             Ok(r) => {
                 Ok(r)
             },
