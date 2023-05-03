bd6efd63322dda434bf552fadd3d353b468350b2
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-03 19:19:49 +0200

    Better practice: use unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ async fn main() {
     helpers::remove_deleted_account().await;
     
     let routes = warp::path("create").and(warp::post()).and(warp::body::json()).and(warp::header("cf-turnstile-token")).and(warp::header::optional::<String>("X-Forwarded-For")).and(warp::addr::remote()).and_then(|body: model::body::Create, cf_token: String, forwarded: Option<String>, ip: Option<SocketAddr>| async move {
-        match router::create::create(body, forwarded.unwrap_or(ip.unwrap_or_else(|| SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)).ip().to_string()), cf_token).await {
+        match router::create::create(body, forwarded.unwrap_or_else(|| ip.unwrap_or_else(|| SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)).ip().to_string()), cf_token).await {
             Ok(r) => {
                 Ok(r)
             },
@@ -67,7 +67,7 @@ async fn main() {
         }
     })
     .or(warp::path("login").and(warp::post()).and(warp::body::json()).and(warp::header("cf-turnstile-token")).and(warp::header::optional::<String>("X-Forwarded-For")).and(warp::addr::remote()).and_then(|body: model::body::Login, cf_token: String, forwarded: Option<String>, ip: Option<SocketAddr>| async move {
-        match router::login::main::login(body, forwarded.unwrap_or(ip.unwrap_or_else(|| SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)).ip().to_string()), cf_token).await {
+        match router::login::main::login(body, forwarded.unwrap_or_else(|| ip.unwrap_or_else(|| SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)).ip().to_string()), cf_token).await {
             Ok(r) => {
                 Ok(r)
             },
