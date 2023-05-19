ee73f9cb410ea0b6686bb7a15103fec149929ee7
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-19 16:30:00 +0200

    Update JWT time
    - 7 days max.

--- src/helpers/jwt.rs
+++ src/helpers/jwt.rs
@@ -1,17 +1,19 @@
 use jsonwebtoken::{encode, decode, Header, Algorithm, EncodingKey, Validation, DecodingKey, TokenData};
 use std::time::{SystemTime, UNIX_EPOCH};
 use serde::{Serialize, Deserialize};
+use anyhow::Result;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: String,
-    exp: u64,
+    pub scope: Vec<String>,
+    pub exp: u64,
     iss: String,
     iat: u64
 }
 
 /// Create a JWT token
-pub fn create_jwt(user_id: String) -> String {
+pub fn create_jwt(user_id: String, scope: Vec<String>) -> String {
     let private_key = EncodingKey::from_rsa_pem(dotenv::var("RSA_PRIVATE_KEY").expect("Missing env `RSA_PRIVATE_KEY`").as_bytes())
         .expect("Failed to load private key");
     let now = SystemTime::now()
@@ -20,18 +22,19 @@ pub fn create_jwt(user_id: String) -> String {
 
     encode(&Header::new(Algorithm::RS256), &Claims {
         sub: user_id.to_lowercase(),
-        exp: now.as_secs() + 5259600,
+        scope,
+        exp: now.as_secs() + 604800,
         iss: "https://oauth.gravitalia.com".to_string(),
         iat: now.as_secs()
     }, &private_key).unwrap()
 }
 
 /// Decode a JWT token and check if it is valid
-pub fn get_jwt(token: String) -> Result<TokenData<Claims>, String> {
+pub fn get_jwt(token: String) -> Result<TokenData<Claims>> {
     let public_key = DecodingKey::from_rsa_pem(dotenv::var("RSA_PUBLIC_KEY").expect("Missing env `RSA_PUBLIC_KEY`").as_bytes())
         .expect("Failed to load public key");
-    decode::<Claims>(&token, &public_key, &Validation::new(Algorithm::RS256))
-        .map_err(|err| err.to_string())
+
+    Ok(decode::<Claims>(&token, &public_key, &Validation::new(Algorithm::RS256))?)
 }
 
 #[cfg(test)]
@@ -40,6 +43,6 @@ mod tests {
 
     #[tokio::test]
     async fn test_jwt() {
-        assert!(regex::Regex::new(r"(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)").unwrap().is_match(&create_jwt("test".to_string())));
+        assert!(regex::Regex::new(r"(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)").unwrap().is_match(&create_jwt("test".to_string(), vec![])));
     }
 }
