00962008d8a1c5894367e89b8eb33ad0dc2847a1
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-06-02 20:30:40 +0200

    Update suspend query struct

--- src/model/query.rs
+++ src/model/query.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 
+/// Query struct for suspend account
 #[derive(Deserialize)]
 pub struct Suspend {
-    pub vanity: String
+    pub vanity: String,
+    pub suspend: Option<bool>
 }
