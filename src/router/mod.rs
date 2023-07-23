75e82a0a1ee45960eec5c313508e9c2978da3396
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-23 20:08:52 +0200

    Add middleware

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,6 +1,35 @@
 pub mod create;
+pub mod users;
 
 use warp::reply::{WithStatus, Json};
+use regex::Regex;
+
+lazy_static! {
+    static ref TOKEN: Regex = Regex::new(r"(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)").unwrap();
+}
+
+/// Check if a token is valid and if have a real user behind (not suspended)
+async fn middleware(scylla: std::sync::Arc<scylla::Session>, token: Option<String>, fallback: &str) -> anyhow::Result<String> {
+    match token {
+        Some(ntoken) if fallback == "@me" => {
+            match crate::helpers::token::check(scylla, ntoken).await {
+                Ok(data) => {
+                    return Ok(data);
+                },
+                Err(e) => {
+                    if e.to_string() == *"revoked" {
+                        return Ok("Suspended".to_string());
+                    } else if e.to_string() == *"expired" {
+                        return Ok("Invalid".to_string());
+                    }
+                }
+            }
+            Ok("Invalid".to_string())
+        }
+        None if fallback == "@me" => Ok("Invalid".to_string()),
+        _ => Ok(fallback.to_string()),
+    }
+}
 
 /// Create message error easier
 fn err(message: String) -> WithStatus<Json> {
@@ -18,7 +47,7 @@ fn rate() -> WithStatus<Json> {
     warp::reply::with_status(warp::reply::json(
         &crate::model::error::Error{
             error: true,
-            message: "Too many requests".to_string(),
+            message: "Rate limit exceeded".to_string(),
         }
     ),
     warp::http::StatusCode::TOO_MANY_REQUESTS)
