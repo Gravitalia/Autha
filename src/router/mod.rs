bf081086f09e89a7b581d9ef120e958228db0246
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-24 15:40:14 +0200

    Add new routes

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,23 +1,30 @@
-pub mod suspend;
 pub mod create;
-pub mod users;
 pub mod login;
+pub mod oauth;
+pub mod suspend;
+pub mod users;
 
-use warp::reply::{WithStatus, Json};
 use regex::Regex;
+use warp::reply::{Json, WithStatus};
 
 lazy_static! {
-    static ref TOKEN: Regex = Regex::new(r"(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)").unwrap();
+    static ref TOKEN: Regex =
+        Regex::new(r"(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)")
+            .unwrap();
 }
 
 /// Check if a token is valid and if have a real user behind (not suspended)
-async fn middleware(scylla: std::sync::Arc<scylla::Session>, token: Option<String>, fallback: &str) -> anyhow::Result<String> {
+async fn middleware(
+    scylla: std::sync::Arc<scylla::Session>,
+    token: Option<String>,
+    fallback: &str,
+) -> anyhow::Result<String> {
     match token {
         Some(ntoken) if fallback == "@me" => {
             match crate::helpers::token::check(scylla, ntoken).await {
                 Ok(data) => {
                     return Ok(data);
-                },
+                }
                 Err(e) => {
                     if e.to_string() == *"revoked" {
                         return Ok("Suspended".to_string());
@@ -35,22 +42,22 @@ async fn middleware(scylla: std::sync::Arc<scylla::Session>, token: Option<Strin
 
 /// Create message error easier
 fn err(message: String) -> WithStatus<Json> {
-    warp::reply::with_status(warp::reply::json(
-        &crate::model::error::Error{
+    warp::reply::with_status(
+        warp::reply::json(&crate::model::error::Error {
             error: true,
             message,
-        }
-    ),
-    warp::http::StatusCode::BAD_REQUEST)
+        }),
+        warp::http::StatusCode::BAD_REQUEST,
+    )
 }
 
 /// Return an error with rate limit informations
 fn rate() -> WithStatus<Json> {
-    warp::reply::with_status(warp::reply::json(
-        &crate::model::error::Error{
+    warp::reply::with_status(
+        warp::reply::json(&crate::model::error::Error {
             error: true,
             message: "Rate limit exceeded".to_string(),
-        }
-    ),
-    warp::http::StatusCode::TOO_MANY_REQUESTS)
+        }),
+        warp::http::StatusCode::TOO_MANY_REQUESTS,
+    )
 }
---
+++ src/router/oauth.rs
@@ -0,0 +1,239 @@
+use crate::helpers::random_string;
+use crate::{
+    database::{
+        mem::{del, get, set, SetValue::Characters},
+        scylla::{create_oauth, query},
+    },
+    helpers,
+};
+use anyhow::{anyhow, Result};
+use std::sync::Arc;
+use warp::reply::{Json, WithStatus};
+
+// Set rust queries
+const GET_BOT: &str = "SELECT flags, deleted, redirect_url, client_secret FROM accounts.bots WHERE id = ?;";
+const GET_OAUTH: &str =
+    "SELECT id, bot_id FROM accounts.oauth WHERE user_id = ?";
+
+/// Handle post request for /oauth
+pub async fn post(
+    scylla: Arc<scylla::Session>,
+    memcached: memcache::Client,
+    body: crate::model::body::OAuth,
+    vanity: String,
+) -> Result<WithStatus<Json>> {
+    let bot = query(Arc::clone(&scylla), GET_BOT, vec![body.bot_id.clone()])
+        .await?
+        .rows
+        .unwrap_or_default();
+
+    // Check if bot exists or deleted
+    if bot.is_empty() {
+        return Ok(super::err("Invalid bot_id".to_string()));
+    } else if bot[0].columns[1]
+        .as_ref()
+        .ok_or_else(|| anyhow!("No reference"))?
+        .as_boolean()
+        .ok_or_else(|| anyhow!("Can't convert to bool"))?
+    {
+        return Ok(super::err("This bot has been deleted".to_string()));
+    }
+
+    // Check if the redirect_url is valid
+    if !bot[0].columns[2]
+        .as_ref()
+        .ok_or_else(|| anyhow!("No reference"))?
+        .as_set()
+        .ok_or_else(|| anyhow!("Can't convert to vec"))?
+        .to_vec()
+        .iter()
+        .any(|x| {
+            x.as_text()
+                .ok_or_else(|| anyhow!("Can't convert to string"))
+                .unwrap()
+                == &&body.redirect_uri[..]
+        })
+    {
+        return Ok(super::err("Invalid redirect_uri".to_string()));
+    }
+
+    // If empty, tries to find a valid code
+    if body.response_type.is_empty() {
+        let res = query(scylla, GET_OAUTH, vec![vanity.clone()])
+            .await?
+            .rows
+            .unwrap_or_default();
+
+        if res.is_empty() {
+            Ok(super::err("".to_string()))
+        } else {
+            if res
+                .iter()
+                .filter(|x| {
+                    *x.columns[1]
+                        .as_ref()
+                        .ok_or_else(|| anyhow!("No reference"))
+                        .unwrap()
+                        .as_text()
+                        .ok_or_else(|| anyhow!("Can't convert to string"))
+                        .unwrap()
+                        == body.bot_id
+                })
+                .map(|x| {
+                    x.columns[0]
+                        .as_ref()
+                        .ok_or_else(|| anyhow!("No reference"))
+                        .unwrap()
+                        .as_text()
+                        .ok_or_else(|| anyhow!("Can't convert to string"))
+                        .unwrap()
+                        .to_string()
+                })
+                .collect::<String>()
+                .is_empty()
+            {
+                return Ok(super::err("".to_string()));
+            }
+
+            let id = random_string(24);
+            let _ = set(
+                memcached.clone(),
+                id.clone(),
+                Characters(format!(
+                    "{}+{}+{}",
+                    body.bot_id, body.redirect_uri, vanity
+                )),
+            );
+
+            Ok(warp::reply::with_status(
+                warp::reply::json(&crate::model::error::Error {
+                    error: false,
+                    message: id,
+                }),
+                warp::http::StatusCode::OK,
+            ))
+        }
+    } else if body
+        .scope
+        .split_whitespace()
+        .filter(|x| !["identity" /*, "private" */].contains(x))
+        .any(|_| true)
+    {
+        Ok(super::err("Invalid scope".to_string()))
+    } else {
+        let id = random_string(24);
+        let _ = set(
+            memcached,
+            id.clone(),
+            Characters(format!(
+                "{}+{}+{}+{}",
+                body.bot_id, body.redirect_uri, vanity, body.scope
+            )),
+        );
+
+        Ok(warp::reply::with_status(
+            warp::reply::json(&crate::model::error::Error {
+                error: false,
+                message: id,
+            }),
+            warp::http::StatusCode::OK,
+        ))
+    }
+}
+
+/// Handle JWT creation, code deletation
+pub async fn get_oauth_code(
+    scylla: Arc<scylla::Session>,
+    memcached: memcache::Client,
+    body: crate::model::body::GetOAuth,
+) -> Result<WithStatus<Json>> {
+    let data = match get(memcached.clone(), body.code.clone()).unwrap() {
+        Some(r) => Vec::from_iter(r.split('+').map(|x| x.to_string())),
+        None => vec![],
+    };
+    if data.is_empty() {
+        return Ok(super::err("Invalid code".to_string()));
+    }
+    let user_id = &data[2];
+
+    if *data[0] != body.client_id {
+        return Ok(super::err("Invalid client_id".to_string()));
+    }
+
+    if data[3]
+        .split_whitespace()
+        .filter(|x| !["identity" /*, "private" */].contains(x))
+        .any(|_| true)
+    {
+        return Ok(super::err("Invalid scope".to_string()));
+    }
+
+    let bot = query(Arc::clone(&scylla), GET_BOT, vec![body.client_id.clone()])
+        .await?
+        .rows
+        .unwrap_or_default();
+
+    // Check if bot exists or deleted
+    if bot.is_empty() {
+        return Ok(super::err("Invalid client_id".to_string()));
+    } else if bot[0].columns[1]
+        .as_ref()
+        .ok_or_else(|| anyhow!("No reference"))?
+        .as_boolean()
+        .ok_or_else(|| anyhow!("Can't convert to bool"))?
+    {
+        return Ok(super::err("This bot has been deleted".to_string()));
+    }
+    // Check if the redirect_url is valid
+    if !bot[0].columns[2]
+        .as_ref()
+        .ok_or_else(|| anyhow!("No reference"))?
+        .as_set()
+        .ok_or_else(|| anyhow!("Can't convert to vec"))?
+        .to_vec()
+        .iter()
+        .any(|x| {
+            x.as_text()
+                .ok_or_else(|| anyhow!("Can't convert to string"))
+                .unwrap()
+                == &&body.redirect_uri[..]
+        })
+    {
+        return Ok(super::err("Invalid redirect_uri".to_string()));
+    }
+    // Check if client_secret is valid
+    if *bot[0].columns[3]
+        .as_ref()
+        .ok_or_else(|| anyhow!("No reference"))?
+        .as_text()
+        .ok_or_else(|| anyhow!("Can't convert to string"))?
+        != body.client_secret
+    {
+        return Ok(super::err("Invalid client_secret".to_string()));
+    }
+
+    // Delete used key
+    let _ = del(memcached, body.code);
+
+    // Create JWT & OAuth
+    let jwt = helpers::jwt::create_jwt(
+        user_id.to_string(),
+        data[3].split_whitespace().map(|x| x.to_string()).collect(),
+    );
+    create_oauth(
+        scylla,
+        jwt.clone(),
+        user_id.to_string(),
+        body.client_id.clone(),
+        data[3].split_whitespace().map(|x| x.to_string()).collect(),
+    )
+    .await;
+
+    Ok(warp::reply::with_status(
+        warp::reply::json(&crate::model::error::Error {
+            error: false,
+            message: jwt,
+        }),
+        warp::http::StatusCode::CREATED,
+    ))
+}
