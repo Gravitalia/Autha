9b3f64faf7fc42a56056c9e10b08cce1daeff28c
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-25 16:37:31 +0200

    Add grpc

--- src/helpers/grpc.rs
+++ src/helpers/grpc.rs
@@ -1,4 +1,4 @@
-/*use anyhow::Result;
+use anyhow::Result;
 
 use spinoza::spinoza_client::SpinozaClient;
 use spinoza::UploadRequest;
@@ -14,26 +14,37 @@ pub mod torresix {
 
 /// Check if an avatar contains NSFW content
 pub async fn check_avatar(avatar: Vec<u8>) -> Result<bool> {
-    let mut client = TorreClient::connect(dotenv::var("TORRESIX_URL").unwrap_or_else(|_| "http://[::1]:50051".to_string())).await?;
+    let mut client = TorreClient::connect(
+        std::env::var("TORRESIX_URL")
+            .unwrap_or_else(|_| "http://[::1]:50051".to_string()),
+    )
+    .await?;
 
-    let response = client.torre_predict(tonic::Request::new(TorreRequest {
-        model: 1,
-        data: avatar
-    })).await?;
+    let response = client
+        .torre_predict(tonic::Request::new(TorreRequest {
+            model: 1,
+            data: avatar,
+        }))
+        .await?;
 
     Ok(response.into_inner().message == "nude")
 }
 
 /// Upload avatar to image provider and resize it
 pub async fn upload_avatar(avatar: Vec<u8>) -> Result<String> {
-    let mut client = SpinozaClient::connect(dotenv::var("SPINOZA_URL").unwrap_or_else(|_| "http://[::1]:28717".to_string())).await?;
+    let mut client = SpinozaClient::connect(
+        std::env::var("SPINOZA_URL")
+            .unwrap_or_else(|_| "http://[::1]:28717".to_string()),
+    )
+    .await?;
 
-    let response = client.upload(tonic::Request::new(UploadRequest {
-        data: avatar,
-        width: 256,
-        height: 0
-    })).await?;
+    let response = client
+        .upload(tonic::Request::new(UploadRequest {
+            data: avatar,
+            width: 256,
+            height: 0,
+        }))
+        .await?;
 
     Ok(response.into_inner().message)
 }
-*/
