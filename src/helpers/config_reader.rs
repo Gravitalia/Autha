1a57e6bab456e8b2eea1bca58e8e72f0e37ea2b4
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-08-20 19:44:26 +0200

    Remove useless log

--- src/helpers/config_reader.rs
+++ src/helpers/config_reader.rs
@@ -1,14 +1,15 @@
 use crate::model::config::Config;
 
+// Define constant variable for the name of the file
+const FILE_NAME: &str = "config.yaml";
+
 // Read configuration file, and then, returns it
 pub fn read() -> Config {
     let config: Config = serde_yaml::from_reader(
-        std::fs::File::open("config.yaml")
+        std::fs::File::open(FILE_NAME)
             .expect("Could not find config.yaml file"),
     )
     .expect("Could not read values of config.yaml file");
-
-    println!("Configuration read-out complete");
 
     config
 }
