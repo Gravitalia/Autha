221b66197f7fcd38af912a7b3b81a806cbde50c2
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-10-01 18:02:21 +0200

    Add helpers benchmark

---
+++ benches/helpers_benchmark.rs
@@ -0,0 +1,14 @@
+use autha::helpers::crypto::*;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn random_string_benchmark(c: &mut Criterion) {
+    c.bench_function(
+        "random_string 20",
+        |b| b.iter(|| {
+            random_string(20)
+        })
+    );
+}
+
+criterion_group!(benches, random_string_benchmark);
+criterion_main!(benches);
