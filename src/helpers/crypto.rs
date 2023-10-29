9b0b2095d3494ecea312f3d98dcab5b4ff96349b
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-10-29 15:12:01 +0100

    chore: Add CHACHA20-POLY1305 support

--- src/helpers/crypto.rs
+++ src/helpers/crypto.rs
@@ -1,8 +1,47 @@
 use anyhow::Result;
 use argon2::{Config, Variant, Version};
 use rand::rngs::OsRng;
-use rand::Rng;
+use rand::{Rng, RngCore};
 use ring::digest::{Context, SHA256};
+use ring::aead::*;
+use std::num::Wrapping;
+
+/// A generator for producing nonces.
+struct NonceGen {
+    /// The current nonce value, wrapped to handle overflow.
+    current: Wrapping<u128>,
+    /// The initial value of the nonce generator.
+    start: u128,
+}
+
+impl NonceGen {
+    /// Creates a new NonceGen instance with the provided 12-byte start value.
+    fn new(start: [u8; 12]) -> Self {
+        let mut array = [0; 16];
+        array[..12].copy_from_slice(&start);
+        let start = u128::from_le_bytes(array);
+        Self {
+            current: Wrapping(start),
+            start,
+        }
+    }
+}
+
+impl NonceSequence for NonceGen {
+    fn advance(&mut self) -> Result<Nonce, ring::error::Unspecified> {
+        // ! Warning: doesn't check u128 overflow correctly
+        // Also, ring docs explicitly call this
+        // out as "reasonable (but probably not ideal)"
+        let n = self.current.0;
+        self.current += 1;
+        if self.current.0 == self.start {
+            return Err(ring::error::Unspecified);
+        }
+        Ok(Nonce::assume_unique_for_key(
+            n.to_le_bytes()[..12].try_into().unwrap(),
+        ))
+    }
+}
 
 /// Generate random string with thread-local cryptographically-secure PRNG seeded from the system's entropy pool.
 pub fn random_string(length: usize) -> String {
@@ -72,6 +111,67 @@ pub fn sha256_digest(data: &[u8]) -> Result<String> {
     Ok(hex::encode(context.finish()))
 }
 
+/// Encrypts the provided data using the CHACHA20_POLY1305 algorithm.
+/// Returns a tuple containing the nonce as a hexadecimal string and the encrypted data as a hexadecimal string.
+pub fn chacha20_poly1305_encrypt(mut data: Vec<u8>) -> Result<(String, String)> {
+    // Get CHACHA20 key. The key MUST be 32 bytes (256 bits), otherwise it panics.
+    let key = std::env::var("CHACHA20_KEY").unwrap_or_default();
+    let key_bytes: &[u8] = if key.is_empty() {
+        &[0; 32]
+    } else {
+        key.as_bytes()
+    };
+
+    // Generate crypto-secure 12 random bytes.
+    let mut os_rng = OsRng;
+    let mut nonce_seed = [0u8; 12];
+    os_rng.fill_bytes(&mut nonce_seed);
+
+    let mut sealing_key = {
+        let key = UnboundKey::new(&CHACHA20_POLY1305, &key_bytes).unwrap();
+        let nonce_gen = NonceGen::new(nonce_seed);
+        SealingKey::new(key, nonce_gen)
+    };
+
+    sealing_key
+        .seal_in_place_append_tag(Aad::empty(), &mut data)
+        .unwrap();
+
+    Ok((hex::encode(nonce_seed), hex::encode(data)))
+}
+
+/// Decrypts the provided data using the CHACHA20_POLY1305 algorithm.
+/// Returns the decrypted data as a string, or default string if decryption fails.
+pub fn chacha20_poly1305_decrypt(
+    nonce: [u8; 12],
+    mut data: Vec<u8>,
+) -> Result<String, std::string::FromUtf8Error> {
+    // Get CHACHA20 key. The key MUST be 32 bytes (256 bits), otherwise it panics.
+    let key = std::env::var("CHACHA20_KEY").unwrap_or_default();
+    let key_bytes: &[u8] = if key.is_empty() {
+        &[0; 32]
+    } else {
+        key.as_bytes()
+    };
+
+    let mut opening_key = {
+        let key = UnboundKey::new(&CHACHA20_POLY1305, key_bytes).unwrap();
+        let nonce_gen = NonceGen::new(nonce);
+        OpeningKey::new(key, nonce_gen)
+    };
+
+    match opening_key.open_in_place(Aad::empty(), &mut data) {
+        Ok(res) => String::from_utf8(res.to_vec()),
+        Err(_) => {
+            log::error!(
+                "Cannot decrypt ChaCha20-Poly1205 data, got an error during opening. Have you decoded hex before call the function?"
+            );
+
+            Ok("".to_string())
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -110,5 +210,25 @@ mod tests {
             hash.unwrap(),
             "8fced00b6ce281456d69daef5f2b33eaf1a4a29b5923ebe5f1f2c54f5886c7a3".to_string()
         );
+    }
+
+    #[tokio::test]
+    async fn test_chacha20_poly1305() {
+        let plaintext = "2018-07-22";
+
+        // Encrypt and get nonce and encrypted text.
+        let (hex_nonce, encrypted) =
+            chacha20_poly1305_encrypt(plaintext.as_bytes().to_vec()).unwrap_or_default();
+
+        // Convert nonce as hex to nonce in 12-bytes.
+        let nonce: [u8; 12] = hex::decode(hex_nonce)
+            .unwrap_or_default()
+            .try_into()
+            .unwrap_or_default();
+        let decrypted =
+            chacha20_poly1305_decrypt(nonce, hex::decode(encrypted).unwrap_or_default())
+                .unwrap_or_default();
+
+        assert_eq!(decrypted, plaintext.to_string())
     }
 }
