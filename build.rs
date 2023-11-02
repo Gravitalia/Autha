00ce800d9c674e9be241f486c8ca5b158d3418f1
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-11-02 16:31:02 +0100

    Delete (#197)
    * Delete benches/helpers_benchmark.rs
    
    * Delete proto/spinoza.proto
    
    * Delete proto/torresix.proto
    
    * Delete build.rs
    
    * Delete src/database/memcached.rs
    
    * Delete src/database/mod.rs
    
    * Delete src/database/scylla.rs
    
    * Delete src/helpers/config.rs
    
    * Delete src/helpers/crypto.rs
    
    * Delete src/helpers/format.rs
    
    * Delete src/helpers/mod.rs
    
    * Delete src/model/body.rs
    
    * Delete src/model/config.rs
    
    * Delete src/model/error.rs
    
    * Delete src/model/mod.rs
    
    * Delete src/router/create.rs
    
    * Delete src/router/mod.rs
    
    * Delete src/lib.rs
    
    * Delete src/main.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .compile(
-                &["proto/torresix.proto", "proto/spinoza.proto"],
-                &["proto"]
-        )
-        .unwrap();
-    Ok(())
-}
--- src/model/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod config;
-pub mod error;
-pub mod body;
