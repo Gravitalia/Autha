12a3622e2690a49b0fc72210f27e079ba0671aa5
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-11-04 15:41:28 +0100

    chore: Add body checker and global rate limiter

--- autha/src/router/create.rs
+++ autha/src/router/create.rs
@@ -1,8 +1,10 @@
 use anyhow::Result;
 use crypto::{hash::sha256, random_string};
-use db::{memcache::MemcachePool, scylla::Scylla};
+use db::{
+    memcache::{MemcacheManager, MemcachePool},
+    scylla::Scylla,
+};
 use regex::Regex;
-use tokio::task;
 use warp::reply::{Json, WithStatus};
 
 lazy_static! {
@@ -17,7 +19,7 @@ lazy_static! {
         Regex::new(r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$").unwrap();
 }
 
-/// Handle create route and check if everything is valid
+/// Handle create route and check if everything is valid.
 pub async fn handle(
     scylla: std::sync::Arc<Scylla>,
     memcached: MemcachePool,
@@ -25,6 +27,54 @@ pub async fn handle(
     ip: String,
     token: Option<String>,
 ) -> Result<WithStatus<Json>> {
+    // Use tokio task if async do not work.
+    // Email verification via regex.
+    if !EMAIL.is_match(&body.email) {
+        return Ok(super::err("Invalid email"));
+    }
+    // Check password length and its reliability.
+    if body.password.len() < 8 && !PASSWORD.is_match(&body.password) {
+        return Ok(super::err("Invalid password"));
+    }
+    // Vanity verification.
+    if !VANITY.is_match(&body.vanity) || body.vanity.chars().all(|c| c.is_ascii_digit()) {
+        return Ok(super::err("Invalid vanity"));
+    }
+    // Username length check.
+    if body.username.len() > 25 {
+        return Ok(super::err("Invalid username"));
+    }
+
+    // Hash IP.
+    let hashed_ip = sha256(ip.as_bytes()).unwrap_or_default();
+    if hashed_ip.is_empty() {
+        log::warn!(
+            "The IP could not be hashed. This can result in the uncontrolled creation of accounts."
+        );
+    }
+
+    // Check if user have already created account 5 minutes ago.
+    let rate_limit = match memcached.get(format!("account_create_{}", hashed_ip))? {
+        Some(r) => r.parse::<u16>().unwrap_or(0),
+        None => 0,
+    };
+    if rate_limit >= 1 {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&crate::model::error::Error {
+                error: true,
+                message: super::ERROR_RATE_LIMITED.into(),
+            }),
+            warp::http::StatusCode::TOO_MANY_REQUESTS,
+        ));
+    }
+
+    // Set to one to the global rate limit.
+    // One because it can only create ONE account each five minutes.
+    memcached.set(
+        format!("account_create_{}", hashed_ip),
+        1,
+    )?;
+
     Ok(warp::reply::with_status(
         warp::reply::json(&crate::model::error::Error {
             error: false,
