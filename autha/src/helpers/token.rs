137077c3d0364121540ebc0962cb176df8387fd9
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-09 16:06:33 +0100

    chore: add get function to check tokens

--- autha/src/helpers/token.rs
+++ autha/src/helpers/token.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use db::scylla::Scylla;
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -48,4 +48,32 @@ pub async fn create(scylla: &Arc<Scylla>, user_id: &String, ip: String) -> Resul
         .await?;
 
     Ok(id)
+}
+
+/// Verify the existence of a user token into database.
+/// Result in error or datas about the token.
+pub async fn get(scylla: &Arc<Scylla>, token: String) -> Result<String> {
+    let rows = scylla
+        .connection
+        .query(
+            "SELECT expire_at, user_id, deleted FROM accounts.tokens WHERE id = ?",
+            vec![token],
+        )
+        .await?
+        .rows_typed::<(i64, String, bool)>()?
+        .collect::<Vec<_>>();
+
+    if rows.is_empty() {
+        bail!("no token exists")
+    }
+
+    let (expire, vanity, deleted) = rows[0].clone().unwrap();
+
+    if expire as u128 <= SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis() {
+        bail!("expired token");
+    } else if deleted {
+        bail!("revoked token")
+    }
+
+    Ok(vanity)
 }
