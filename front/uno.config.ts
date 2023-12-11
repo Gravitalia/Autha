90bca1b322adf904f4803ac4bb7ad58e672fa3bd
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-11 12:05:04 +0100

    feat: add new shortcuts

--- front/uno.config.ts
+++ front/uno.config.ts
@@ -17,5 +17,10 @@ export default defineConfig({
   ],
   shortcuts: {
     "blur-5xl": "blur-300px",
+    "bg-primary": "bg-violet-700 dark:bg-violet-500",
+    "bg-secondary": "bg-amber-400 dark:bg-amber-600",
+
+    "btn-base":
+      "text-sm px-4 py-2 cursor-pointer outline-none border-none rounded bg-primary hover:bg-violet-800 dark:hover:bg-violet-600 font-medium text-white disabled:opacity-75 transition-colors duration-200 disabled:cursor-not-allowed",
   },
 });
