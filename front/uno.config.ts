bccd3d877e949b1188e97eae3148401197950772
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-12 18:34:04 +0100

    chore: add new shortcut for text inputs

--- front/uno.config.ts
+++ front/uno.config.ts
@@ -16,13 +16,16 @@ export default defineConfig({
     }),
   ],
   shortcuts: {
-    "blur-5xl": "blur-300px",
+    "blur-5xl": "blur-240px",
     "bg-primary": "bg-violet-700 dark:bg-violet-500",
     "bg-secondary": "bg-amber-400 dark:bg-amber-600",
 
     "btn-base":
       "text-sm px-4 py-2 cursor-pointer outline-none border-none rounded bg-primary hover:bg-violet-800 dark:hover:bg-violet-600 font-medium text-white disabled:opacity-75 transition-colors duration-200 disabled:cursor-not-allowed",
 
-    "container": "flex justify-center items-center"
+    input:
+      "w-64 h-7 text-zinc-700 dark:text-zinc-200 dark:placeholder:text-zinc-300 outline-none bg-transparent border-solid border-0 border-zinc-400 dark:border-zinc-700 border-b-2",
+
+    container: "flex justify-center items-center",
   },
 });
