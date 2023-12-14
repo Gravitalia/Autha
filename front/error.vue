0ba0e626d3842d240becb8b39eaf8f31d685f3a1
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-14 15:17:55 +0100

    fix: disable eslint

--- front/error.vue
+++ front/error.vue
@@ -2,27 +2,32 @@
 import type { NuxtError } from "#app";
 
 const { error } = defineProps<{
-  error: Partial<NuxtError>
+  error: Partial<NuxtError>;
 }>();
 
 const errorCodes: Record<number, string> = {
   404: "Page not found",
 };
 
+/* eslint-disable no-console */
 if (process.dev) console.error(error);
 </script>
 
 <template>
   <NuxtLayout>
-      <template #title>
-        <span timeline-title-style>Error</span>
-      </template>
-      <slot>
-        <div h-screen font-sans flex-col container>
-            <h1 text-2xl>{{ $t(errorCodes[error.statusCode!] || "Something went wrong") }}</h1>
+    <template #title>
+      <span timeline-title-style>Error</span>
+    </template>
+    <slot>
+      <div h-screen font-sans flex-col container>
+        <h1 text-2xl>
+          {{ $t(errorCodes[error.statusCode!] || "Something went wrong") }}
+        </h1>
 
-            <NuxtLink to="/" prefetch btn-base no-underline mt-11>{{ $t("Return home") }}</NuxtLink>
-        </div>
-      </slot>
+        <NuxtLink to="/" prefetch btn-base no-underline mt-11>{{
+          $t("Return home")
+        }}</NuxtLink>
+      </div>
+    </slot>
   </NuxtLayout>
 </template>
