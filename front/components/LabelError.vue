37851f30f2a712d651efdcadd8218f5b2d7f7f00
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-22 17:08:37 +0100

    feat: reduce vue size using components

---
+++ front/components/LabelError.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  text: string;
+  cond?: boolean;
+}>();
+</script>
+
+<template>
+  <label v-if="cond ?? true" absolute text-sm text-red-500 w-64>
+    {{ $t(text) }}
+  </label>
+</template>
