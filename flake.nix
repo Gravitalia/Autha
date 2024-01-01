c660580b63917d5bf102214b32ace598a31a2006
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2024-01-01 14:23:18 +0100

    Refactor: Update flake.nix for improved readability and platform specificity
    Adjusted the structure of `flake.nix` to enhance readability by reformatting some lines and optimizing the conditional inclusion of framework dependencies based on the operating system. The changes aim to clarify the structure and ensure proper inclusion of dependencies for different platforms.
    
    contact@l31.dev

--- flake.nix
+++ flake.nix
@@ -1,28 +1,26 @@
 {
-  description = "Set up a devShell for a Rust project with necessary dependencies.";
+  description =
+    "Set up a devShell for a Rust project with necessary dependencies.";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; 
+      let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ 
-            cargo
-            rustc
-            pkg-config
-            openssl
-            protobuf
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-            darwin.apple_sdk.frameworks.CoreServices
-            libiconv
-          ];
+          buildInputs = with pkgs;
+            [ cargo rustc pkg-config openssl protobuf libiconv ]
+            ++ (if lib.strings.hasInfix system "darwin" then [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+              darwin.apple_sdk.frameworks.CoreServices
+            ] else
+              [ ]);
         };
-      }
-    );
+      });
 }
+
