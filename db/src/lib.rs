9d8fec1f0d96a2107d0487ee1bcabd2493e9c97f
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-03 00:49:13 +0100

    doc: add missing doc

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,3 +1,36 @@
+// Clippy lint.
+#![deny(missing_docs)]
+//! # db
+//!
+//! library to simply use databases.
+//!
+//! Supported databases:
+//! - memcached;
+//! - scylladb/cassandra.
+//! Supported message broker:
+//! - kafka/redpanda;
+//!
+//! # Init memcached database.
+//!
+//! ```rust
+//! let memcached_pool = match db::memcache::init(
+//!     vec!["localhost:11211".to_string()],
+//!     15,
+//! ) {
+//!     Ok(pool) => {
+//!         db::memcache::MemcachePool {
+//!             connection: Some(pool),
+//!         }
+//!     }
+//!     Err(error) => {
+//!         panic!("");
+//!     }
+//! };
+//! ```
+
+/// Memcached database handler.
 pub mod memcache;
+/// Models requiring databases traits.
 pub mod model;
+/// Scylla database handler.
 pub mod scylla;
