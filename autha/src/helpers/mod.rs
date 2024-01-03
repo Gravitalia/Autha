bc97c2ecaca5cc9f8b391f26aec6203a9454f001
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-03 01:22:27 +0100

    fix: get age function works again (#269)
    * chore(deps): add chrono crate
    
    * chore(bazel): add chrono crate on lockfile
    
    * chore(deps): add chrono crate
    
    * fix: use chrono instead of scratch
    
    - Use chrono instead of home made and bugged get age implementation.
    - Fix test with the new year (happy new year).
    
    * fix: remove zero-prefixed literal
    
    Fix dependabot issue "Found zero-prefixed literal" by removing them.

--- autha/src/helpers/mod.rs
+++ autha/src/helpers/mod.rs
@@ -4,54 +4,28 @@ pub mod machine_learning;
 pub mod request;
 pub mod token;
 
+use anyhow::Result;
+use chrono::prelude::*;
 use std::time::{SystemTime, UNIX_EPOCH};
+
+const MILLIS_IN_YEAR: u128 = 31_556_952_000;
 
 /// Get age with given year, month and day.
 /// ```rust
 /// assert_eq!(get_age(2000, 01, 29), 23f64);
 /// ```
-pub fn get_age(year: i16, month: i8, day: i8) -> anyhow::Result<i32> {
-    // Calculating duration since the UNIX era
-    let duration_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH)?;
+pub fn get_age(year: i16, month: i8, day: i8) -> Result<i32> {
+    let current_time = SystemTime::now().duration_since(UNIX_EPOCH)?;
 
-    // Calculate the number of days spent
-    let days_since_epoch = duration_since_epoch.as_secs() / 86_400 - 12;
+    let birth_date = NaiveDate::from_ymd_opt(year.into(), month.try_into()?, day.try_into()?)
+        .unwrap_or_default()
+        .and_hms_milli_opt(0, 0, 0, 0)
+        .unwrap_or_default()
+        .and_local_timezone(Utc)
+        .unwrap()
+        .timestamp_millis() as u128;
 
-    // Calculate the year, month and day based on the number of days past
-    let years_since_epoch: i16 = (1970 + days_since_epoch / 365)
-        .try_into()
-        .unwrap_or_default();
-    let mut days_in_year = days_since_epoch % 365;
-    let mut current_month = 0;
-    let mut current_day = 0;
-    let days_in_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    // Find the current month and day based on days passed in the year
-    for (i, &days_in_month) in days_in_months.iter().enumerate() {
-        days_in_year -= days_in_month;
-
-        if days_in_year <= days_in_month {
-            current_month = i as i8 + 2;
-            current_day = days_in_year.try_into()?;
-            break;
-        }
-    }
-
-    // Compare birthdate with current date
-    if year > years_since_epoch
-        || (year == years_since_epoch && month > current_month)
-        || (year == years_since_epoch && month == current_month && day > current_day)
-    {
-        Ok(0)
-    } else {
-        let mut age = years_since_epoch - year;
-
-        if month > current_month || (month == current_month && day > current_day) {
-            age -= 1;
-        }
-
-        Ok(age.into())
-    }
+    Ok(((current_time.as_millis() - birth_date) / MILLIS_IN_YEAR).try_into()?)
 }
 
 #[cfg(test)]
@@ -60,6 +34,6 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_age() {
-        assert_eq!(get_age(2000, 1, 29).unwrap(), 23);
+        assert_eq!(get_age(2000, 1, 1).unwrap(), 24);
     }
 }
