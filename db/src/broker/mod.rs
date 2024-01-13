f840b8fd0849471c0efac39c35996fbff71c02a4
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-13 23:35:32 +0100

    feat: add rabbitmq support (#279)
    * chore(deps): add lapin
    
    * chore(bazel): update lockfile
    
    * add rabbitmq manager 
    
    - add pool manager
    - add rabbitmq pool manager
    
    * allow creation of rabbitmq pool
    
    * use rabbitmq
    
    * bazel
    
    * add rabbitmq to the config

--- db/src/broker/mod.rs
+++ db/src/broker/mod.rs
@@ -2,63 +2,57 @@ use anyhow::Result;
 
 /// `kafka` module contains functionalities related to Kafka communication.
 pub mod kafka;
+/// `rabbitmq` module contains functionalities related to RabbitMQ communication.
+pub mod rabbitmq;
 
 use crate::broker::kafka::KafkaPool;
+use crate::broker::rabbitmq::RabbitPool;
 
 /// Enumerates the possibilities of usable broker messages.
+#[derive(Default, Clone)]
 pub enum Broker {
     /// Stores a Kafka pool of connections.
     Kafka(KafkaPool),
     /// Stores a RabbitMQ pool of connections.
-    RabbitMQ(()),
+    RabbitMQ(RabbitPool),
+    /// Stores a non-connected broker.
+    #[default]
+    None,
 }
 
-impl From<KafkaPool> for Broker {
-    fn from(k: KafkaPool) -> Self {
-        Broker::Kafka(k)
-    }
+/// Create a new empty `Broker` representation.
+pub fn empty() -> Broker {
+    Broker::default()
 }
 
-impl From<()> for Broker {
-    fn from(_r: ()) -> Self {
-        unimplemented!()
-    }
+/// Creates a new instance of `Broker` using Kafka.
+///
+/// # Arguments
+///
+/// * `hosts` - A vector of strings representing Kafka broker hosts.
+/// * `pool_size` - The size of the connection pool for Kafka.
+///
+/// # Returns
+///
+/// A Result containing a new instance of `Broker` with a Kafka connection pool.
+pub fn with_kafka(hosts: Vec<String>, pool_size: u32) -> Result<Broker> {
+    Ok(Broker::Kafka(KafkaPool {
+        connection: kafka::init(hosts, pool_size)?,
+    }))
 }
 
-/// Structure representing a set of brokers.
-#[derive(Debug, Clone)]
-pub struct Brokers<B: Into<Broker> + Clone> {
-    /// The optional broker instance.
-    pub broker: Option<B>,
-    /// The list of hosts for the brokers.
-    pub hosts: Vec<String>,
-    /// The size of the connection pool.
-    pub pool_size: u32,
-}
-
-impl<B> Brokers<B>
-where
-    B: Into<Broker> + Clone,
-{
-    /// Creates a new instance of `Brokers`.
-    pub fn new(hosts: Vec<String>, pool_size: u32) -> Self {
-        Brokers {
-            broker: None,
-            hosts,
-            pool_size,
-        }
-    }
-
-    /// Creates a new instance of `Brokers` using Kafka.
-    pub fn with_kafka(hosts: Vec<String>, pool_size: u32) -> Result<Brokers<KafkaPool>> {
-        let new_kafka_connection = kafka::init(hosts.clone(), pool_size)?;
-
-        Ok(Brokers {
-            broker: Some(kafka::KafkaPool {
-                connection: new_kafka_connection,
-            }),
-            hosts,
-            pool_size,
-        })
-    }
+/// Creates a new instance of `Broker` using RabbitMQ.
+///
+/// # Arguments
+///
+/// * `hosts` - A vector of strings representing RabbitMQ broker hosts.
+/// * `pool_size` - The size of the connection pool for RabbitMQ.
+///
+/// # Returns
+///
+/// A Result containing a new instance of `Broker` with a RabbitMQ connection pool.
+pub fn with_rabbitmq(hosts: Vec<String>, pool_size: u32) -> Result<Broker> {
+    Ok(Broker::RabbitMQ(RabbitPool {
+        connection: rabbitmq::init(hosts, pool_size)?,
+    }))
 }
