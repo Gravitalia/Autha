0740924df5d7b76fa043965ceb660fa41d93e552
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-27 23:09:23 +0100

    doc: add missing documentation
    also remove useless variables

--- autha/src/model/query.rs
+++ autha/src/model/query.rs
@@ -3,9 +3,10 @@ use serde::Deserialize;
 /// Represents the query structure for creating new authorization token.
 #[derive(Deserialize)]
 pub struct OAuth {
+    /// Application (bot) vanity/unique identifier.
     pub client_id: String,
-    pub response_type: String,
+    /// Redirect URL to be followed by the user.
     pub redirect_uri: String,
+    /// Bot access granted by the user.
     pub scope: String,
-    pub state: String,
 }
