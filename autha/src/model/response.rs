c5875443dbd1e346fb46726c0fbb66945154beaa
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-28 18:00:52 +0100

    feat: add new struct to create access_token

--- autha/src/model/response.rs
+++ autha/src/model/response.rs
@@ -10,3 +10,18 @@ pub struct Token {
     /// Account settings to keep services consistent.
     pub user_settings: super::config::UserSettings,
 }
+
+/// Represents the body structure for creating a new access token for OAuth2.
+#[derive(Serialize)]
+pub struct AccessToken {
+    /// Token to access granted data.
+    pub access_token: String,
+    /// access_token validity period.
+    pub expires_in: u64,
+    /// Token to recreate an acces_token.
+    pub refresh_token: String,
+    /// refresh_token validity period.
+    pub refresh_token_expires_in: u64,
+    /// List of accesses granted.
+    pub scope: String,
+}
