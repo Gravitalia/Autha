f51d80a4ee21fb928dc98f4ace522666289522e6
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-02-18 13:32:33 +0100

    feat: support refresh token

--- autha/src/router/oauth.rs
+++ autha/src/router/oauth.rs
@@ -111,6 +111,9 @@ pub async fn grant(
         "authorization_code" => {
             authorization_code(scylla, memcached, body).await
         },
+        "refresh_token" => {
+            refresh_token(scylla, body).await
+        },
         _ => Ok(super::err("Invalid grant_type")),
     }
 }
@@ -243,6 +246,50 @@ pub async fn authorization_code(
             refresh_token,
             refresh_token_expires_in: 5_184_000, // 60 days.
             scope: scope.to_string(),
+            token_type: "bearer".to_string(),
+        }),
+        warp::http::StatusCode::CREATED,
+    ))
+}
+
+/// Handle creation of `access_token` from a refresh token.
+pub async fn refresh_token(
+    scylla: Arc<Scylla>,
+    body: crate::model::body::OAuth,
+) -> Result<WithStatus<Json>> {
+    let refresh_token = if let Some(token) = body.refresh_token {
+        token
+    } else {
+        return Ok(super::err("Missing refresh_token"));
+    };
+
+    let rows = scylla
+        .connection
+        .query(
+            "SELECT user_id, scope, deleted FROM accounts.oauth WHERE id = ?",
+            vec![&refresh_token],
+        )
+        .await?
+        .rows_typed::<(String, Vec<String>, bool)>()?
+        .collect::<Vec<_>>();
+
+    let (user_id, scopes, deleted) = rows[0].clone()?;
+
+    if deleted {
+        return Ok(super::err("Expired refresh_token"));
+    }
+
+    // Create access token.
+    let (expires_in, access_token) =
+        crate::helpers::token::create_jwt(user_id, scopes.clone())?;
+
+    Ok(warp::reply::with_status(
+        warp::reply::json(&crate::model::response::AccessToken {
+            access_token,
+            expires_in,
+            refresh_token,
+            refresh_token_expires_in: 0,
+            scope: scopes.join(" "),
             token_type: "bearer".to_string(),
         }),
         warp::http::StatusCode::CREATED,
