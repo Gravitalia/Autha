8a6a3acca87a210f739944af3d5653f0294c50b8
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-03-03 17:43:43 +0100

    remove magic string

--- autha/src/router/oauth/mod.rs
+++ autha/src/router/oauth/mod.rs
@@ -13,6 +13,7 @@ use warp::{
 };
 
 const VALID_SCOPE: [&str; 1] = ["identity"];
+const REFRESH_TOKEN_LENGTH: usize = 512;
 
 /// This route allows you to create an `authorization code` and then obtain an access token linked to a user.
 /// This is an implementation made for a grant type specified on `authorization_code`.
