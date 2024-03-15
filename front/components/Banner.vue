f5732406d069c5f675025cf165c6c6281e6d5b39
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-03-15 23:19:19 +0100

    finish banner

--- front/components/Banner.vue
+++ front/components/Banner.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+/* eslint vue/multi-word-component-names: "off", vue/valid-attribute-name: "off" */
+
+defineProps({
+  content: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  canClose: Boolean,
+});
 const isClosed = ref(false);
 </script>
 
@@ -13,9 +23,8 @@ const isClosed = ref(false);
     flex
     items-center
     gap-x-6
-    backdrop-blur-3xl
+    bg-primary
     px-6
-    py-1
     sm:px-3.5
     sm:before:flex-1
   >
@@ -48,30 +57,25 @@ const isClosed = ref(false);
       ></div>
     </div>
     <div xl:ml-8 flex flex-wrap items-center gap-x-4 gap-y-2>
-      <p font-semibold text-sm leading-6>Avertissement</p>
-      <p text-sm leading-6 text-zinc-700 dark:text-zinc-200>
-        Les services de gestion de compte ne sont actuellement pas disponibles.
+      <p text-white font-semibold text-sm leading-6>
+        {{ $t("warning") }}
+      </p>
+      <p text-sm leading-6 text-zinc-200>
+        {{ content }}
       </p>
     </div>
-    <div xl:mr-8 flex flex-1 justify-end>
+    <div v-if="canClose" xl:mr-8 flex flex-1 justify-end>
       <button
         type="button"
-        @click="isClosed = true"
         cursor-pointer
         -m-3
         focus-visible:outline-offset-[-4px]
         bg-transparent
         border-none
+        @click="isClosed = true"
       >
         <span sr-only>Dismiss</span>
-        <svg
-          h-5
-          w-5
-          fill-zinc-900
-          dark:fill-zinc-200
-          viewBox="0 0 20 20"
-          aria-hidden="true"
-        >
+        <svg h-5 w-5 fill-zinc-200 viewBox="0 0 20 20" aria-hidden="true">
           <path
             d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
           />
