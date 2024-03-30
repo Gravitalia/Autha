ca7a580765b0043cbf8fe1256447cbac059532ee
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-03-30 15:03:57 +0100

    add new benchmark format

--- image_processor/benches/image_benchmark.rs
+++ image_processor/benches/image_benchmark.rs
@@ -1,11 +1,49 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use image_processor::resizer::resize;
+use image_processor::resizer::{resize, Encode, Encoder, Lossless};
 
 fn resize_benchmark(c: &mut Criterion) {
     let buffer = &std::fs::read("benches/image.jpg").unwrap();
 
-    c.bench_function("resize ~100KB 100w", |b| {
-        b.iter(|| resize(buffer, Some(100), None))
+    c.bench_function("resize JPEG to PNG ~100KB 100w", |b| {
+        b.iter(|| {
+            resize(
+                buffer,
+                Encoder {
+                    encoder: Encode::Lossless(Lossless::Png),
+                    width: Some(100),
+                    height: None,
+                    speed: None,
+                },
+            )
+        })
+    });
+
+    c.bench_function("resize JPEG to WebP ~100KB 100w", |b| {
+        b.iter(|| {
+            resize(
+                buffer,
+                Encoder {
+                    encoder: Encode::Lossless(Lossless::WebP),
+                    width: Some(100),
+                    height: None,
+                    speed: None,
+                },
+            )
+        })
+    });
+
+    c.bench_function("resize JPEG to AVIF ~100KB 100w", |b| {
+        b.iter(|| {
+            resize(
+                buffer,
+                Encoder {
+                    encoder: Encode::Lossless(Lossless::Avif),
+                    width: Some(100),
+                    height: None,
+                    speed: None,
+                },
+            )
+        })
     });
 }
 
