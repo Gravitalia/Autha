6c147e30a8959b4a8974c95e55ed179329d42173
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-04-06 23:44:57 +0200

    add telemetry and copyright

--- autha/src/main.rs
+++ autha/src/main.rs
@@ -1,6 +1,15 @@
+// Copyright 2024 Gravitalia.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.mozilla.org/en-US/MPL/2.0/
+
 mod helpers;
 mod model;
 mod router;
+mod telemetry;
 
 #[macro_use]
 extern crate lazy_static;
