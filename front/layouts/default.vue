bdbe0be6db0f280766ff57b53ff2e808592c283e
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-04-12 15:52:23 +0200

    add layout

---
+++ front/layouts/default.vue
@@ -0,0 +1,29 @@
+<script setup>
+const { t } = useI18n();
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: "id",
+  addSeoAttributes: true,
+});
+</script>
+
+<template>
+  <div>
+    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+      <Head>
+        <Title>{{ $t("gravitalia_account") }}</Title>
+        <template v-for="link in head.link" :key="link.id">
+          <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <Meta name="og:description" :content="t('gravitalia_description')" />
+        <Meta name="description" :content="t('gravitalia_description')" />
+        <template v-for="meta in head.meta" :key="meta.id">
+            <Meta :id="meta.id" :property="meta.property" :content="meta.property" />
+        </template>
+      </Head>
+      <Body>
+        <slot />
+      </Body>
+    </Html>
+  </div>
+</template>
