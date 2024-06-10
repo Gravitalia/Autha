b5145dd458a66d2afdd58e703e4f62b9e520a53b
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-06-10 19:35:30 +0200

    add translations

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -285,7 +285,7 @@ async function update() {
         "
         draggable="false"
       />
-      
+
       <button
         type="button"
         :disabled="loadingButton"
@@ -296,30 +296,30 @@ async function update() {
         btn-base
         w-full
         @click="avatar ? update() : avatarInput.click()"
-        v-html="
-          avatar
-            ? $t('save')
-            : `
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              w-5
-              h-5
-              px-2
-            >
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z'
-              />
-            </svg>
-            Importer une nouvelle photo
-            `
-        "
-      ></button>
+      >
+        <template v-if="avatar">
+          {{ $t("save") }}
+        </template>
+        <template v-else>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            w-5
+            h-5
+            px-2
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+            />
+          </svg>
+          {{ $t("update.profile_picture.import") }}
+        </template>
+      </button>
     </div>
   </UpdateModal>
 </template>
