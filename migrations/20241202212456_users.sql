37b27ea76687b1dfa296a14362238513adbd37d2
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-26 17:06:05 +0100

    Support délectation

--- migrations/20241202212456_users.sql
+++ migrations/20241202212456_users.sql
@@ -1,14 +1,16 @@
 -- User table logic.
 
 CREATE TABLE IF NOT EXISTS users (
-  vanity      TEXT        PRIMARY KEY,
-  username    TEXT        NOT NULL,
-  email       TEXT        NOT NULL,
-  password    TEXT        NOT NULL,
-  avatar      TEXT,
-  flags       INT         NOT NULL DEFAULT 0,
-  created_at  DATE        NOT NULL DEFAULT NOW(), -- Millisecond precision seems... too precise
-  updated_at  TIMESTAMPTZ
+  vanity        TEXT        PRIMARY KEY,
+  username      TEXT        NOT NULL,
+  email         TEXT        UNIQUE NOT NULL,
+  password      TEXT        NOT NULL,
+  avatar        TEXT,
+  flags         INT         NOT NULL DEFAULT 0,
+  suspended_at  DATE,
+  deleted_at    DATE,
+  created_at    DATE        NOT NULL DEFAULT NOW(), -- Millisecond precision seems... too precise
+  updated_at    TIMESTAMPTZ
 );
 
 -- Create a trigger to automatically update "updated_at" row.
@@ -35,3 +37,16 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT trigger_updated_at('"users"');
+
+-- Define a function to delete users based on grace period.
+CREATE OR REPLACE FUNCTION clean_deleted_users()
+RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM users
+    WHERE (deleted_at IS NOT NULL AND deleted_at + INTERVAL '30 days' < NOW())
+       OR (suspended_at IS NOT NULL AND suspended_at + INTERVAL '15 days' < NOW());
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT cron.schedule('daily_cleanup', '0 0 * * *', 'SELECT clean_deleted_users();');
