30ad2b61aaf59fae022633910d183ec3c5ab394e
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-27 22:47:08 +0100

    JSON status

--- src/router/status.rs
+++ src/router/status.rs
@@ -1,31 +1,10 @@
 //! Public configuration page for front-end identification and customization.
 
-use std::env::var;
+use axum::{extract::State, Json};
 
-use axum::Json;
-use serde::Serialize;
-
-/// Structured configuration.
-#[derive(Serialize)]
-pub struct Status {
-    version: String,
-    name: String,
-    favicon: Option<String>,
-}
+use crate::status::Configuration;
 
 /// Public server status (configuration).
-pub async fn status() -> Json<Status> {
-    Json(Status {
-        version: env!("CARGO_PKG_VERSION").into(),
-        name: if let Ok(name) = var("SERVER_NAME") {
-            name
-        } else {
-            env!("CARGO_CRATE_NAME").into()
-        },
-        favicon: if let Ok(path) = var("SERVER_ICON") {
-            Some(path)
-        } else {
-            None
-        },
-    })
+pub async fn status(State(configuration): State<Configuration>) -> Json<Configuration> {
+    Json(configuration)
 }
