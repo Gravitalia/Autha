692bea5c4b99b9a92eed37aa63523df7e3779da2
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-27 16:14:45 +0100

    Create invite_codes table

---
+++ migrations/20241225194422_invite_codes.sql
@@ -0,0 +1,8 @@
+-- Inivtation table logic.
+
+CREATE TABLE IF NOT EXISTS invite_codes (
+  code        TEXT        PRIMARY KEY,
+  used_by     TEXT        REFERENCES users(vanity)  
+  used_at     TIMESTAMP,
+  created_at  DATE        NOT NULL DEFAULT NOW(),
+);
