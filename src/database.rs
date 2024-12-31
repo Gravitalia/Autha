0d2260a7e2f790b11ab0636d46b1a9048df88660
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-31 21:48:28 +0100

    Add pg

--- src/database.rs
+++ src/database.rs
@@ -1,1 +1,14 @@
+use sqlx::PgPool;
 
+#[derive(Clone)]
+pub struct Database {
+    postgres: PgPool,
+}
+
+impl Database {
+    pub async fn new(pg_url: &str) -> Result<Self, sqlx::Error> {
+        let postgres = PgPool::connect(pg_url).await?;
+
+        Ok(Self { postgres })
+    }
+}
