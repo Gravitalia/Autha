587693ae9b3eb31b437638e0e215e6fb3e3c1397
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-01-01 19:39:06 +0100

    Handle `validator` error

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -3,3 +3,53 @@
 
 pub mod status;
 pub mod create;
+
+use axum::{
+    extract::{rejection::FormRejection, Form, FromRequest, Request},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use serde::de::DeserializeOwned;
+use thiserror::Error;
+use validator::Validate;
+
+/// Custom struct to validate form data.
+#[derive(Debug, Clone, Copy, Default)]
+pub struct ValidatedForm<T>(pub T);
+
+impl<T, S> FromRequest<S> for ValidatedForm<T>
+where
+    T: DeserializeOwned + Validate,
+    S: Send + Sync,
+    Form<T>: FromRequest<S, Rejection = FormRejection>,
+{
+    type Rejection = ServerError;
+
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        let Form(value) = Form::<T>::from_request(req, state).await?;
+        value.validate()?;
+        Ok(ValidatedForm(value))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ServerError {
+    #[error(transparent)]
+    Validation(#[from] validator::ValidationErrors),
+
+    #[error(transparent)]
+    AxumForm(#[from] FormRejection),
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        match self {
+            ServerError::Validation(_) => {
+                let message = format!("{self}").replace('\n', ", ");
+                (StatusCode::BAD_REQUEST, message)
+            }
+            ServerError::AxumForm(_) => (StatusCode::BAD_REQUEST, self.to_string()),
+        }
+        .into_response()
+    }
+}
