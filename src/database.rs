441810245f1227e1eaf3248bf0d1764182b9ff90
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-01-01 14:38:52 +0100

    Add comments

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,16 @@
+//! database (db) union structure.
 use sqlx::PgPool;
 
+pub const DEFAULT_PG_URL: &str = "postgres://postgres:postgres@localhost:5432/autha";
+
+/// Custom db structure to pass to Axum.
 #[derive(Clone)]
 pub struct Database {
     postgres: PgPool,
 }
 
 impl Database {
+    /// Init database connections.
     pub async fn new(pg_url: &str) -> Result<Self, sqlx::Error> {
         let postgres = PgPool::connect(pg_url).await?;
 
