7e5280e536f14636f51d4e4be909b8b8d8ca4096
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-01-09 17:46:26 +0100

    Add custom error message

--- src/router/create.rs
+++ src/router/create.rs
@@ -1,12 +1,17 @@
 use axum::{extract::State, Json};
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 use crate::database::Database;
+use super::ValidatedForm;
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct Body {
+    #[validate(length(min = 2, max = 15))]
     vanity: String,
+    #[validate(email(message = "Email must be formated."))]
     email: String,
+    #[validate(length(min = 8, message = "Password must contain at least 8 characters."))]
     password: String,
 }
 
@@ -17,7 +22,10 @@ pub struct Response {
     locale: String,
 }
 
-pub async fn create(State(db): State<Database>, Json(body): Json<Body>) -> Json<Response> {
+pub async fn create(
+    State(_db): State<Database>,
+    ValidatedForm(_body): ValidatedForm<Body>,
+) -> Json<Response> {
     Json(Response {
         vanity: String::default(),
         token: String::default(),
