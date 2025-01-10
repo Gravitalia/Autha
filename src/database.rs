bd7cbe2015f2141d98a9576d8ccf5c9d3b4439f1
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-01-10 17:53:52 +0100

    Impl substract

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,8 @@
 //! database (db) union structure.
+use axum::extract::FromRef;
 use sqlx::PgPool;
+
+use crate::AppState;
 
 pub const DEFAULT_PG_URL: &str = "postgres://postgres:postgres@localhost:5432/autha";
 
@@ -15,5 +18,11 @@ impl Database {
         let postgres = PgPool::connect(pg_url).await?;
 
         Ok(Self { postgres })
+    }
+}
+
+impl FromRef<AppState> for Database {
+    fn from_ref(app_state: &AppState) -> Database {
+        app_state.db.clone()
     }
 }
