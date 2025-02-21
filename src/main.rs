60d34038b05b6a9e9b4071d0a8c65d34f5c7ad37
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-02-21 19:17:56 +0100

    Update telemetry

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 #[deny(missing_docs, unused_mut)]
 mod crypto;
 mod database;
-mod metrics;
 mod router;
 mod status;
+mod telemetry;
 mod user;
 mod well_known;
 
+use axum::body::Bytes;
 use axum::{
     http::{header, Method},
     middleware,
@@ -15,25 +16,18 @@ use axum::{
     Router,
 };
 use opentelemetry::global;
-use opentelemetry::trace::{Span, Tracer};
-use opentelemetry::KeyValue;
-use opentelemetry_sdk::trace::SdkTracerProvider;
-use opentelemetry_sdk::Resource;
-use tower::{Layer, Service};
+use tower::ServiceBuilder;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::timeout::RequestBodyTimeoutLayer;
 use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::TraceLayer;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse};
+use tower_http::LatencyUnit;
+use tracing_subscriber::{prelude::*, EnvFilter};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use well_known::well_known;
 
 use std::env;
 use std::future::ready;
-use std::future::Future;
-use std::pin::Pin;
-use std::process;
-use std::task::{Context, Poll};
 use std::time::Duration;
 
 #[derive(Clone)]
@@ -41,67 +35,6 @@ pub struct AppState {
     pub config: status::Configuration,
     pub db: database::Database,
 }
-
-#[derive(Clone)]
-struct OTelLayer;
-
-impl<S> Layer<S> for OTelLayer {
-    type Service = OTelService<S>;
-
-    fn layer(&self, inner: S) -> Self::Service {
-        OTelService { inner }
-    }
-}
-
-#[derive(Clone)]
-struct OTelService<S> {
-    inner: S,
-}
-
-impl<Req, S> Service<Req> for OTelService<S>
-where
-    S: Service<Req> + Clone + Send + 'static,
-    S::Future: Send + 'static,
-    Req: Send + 'static,
-{
-    type Response = S::Response;
-    type Error = S::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
-
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
-    }
-
-    fn call(&mut self, req: Req) -> Self::Future {
-        let tracer = global::tracer("tracing-http");
-
-        let mut otel_span = tracer.start("http-request");
-        otel_span.set_attribute(KeyValue::new("type", "http"));
-
-        let fut = self.inner.call(req);
-        Box::pin(async move {
-            let res = fut.await;
-            otel_span.end();
-            res
-        })
-    }
-}
-
-fn tracer() -> SdkTracerProvider {
-    let exporter = opentelemetry_otlp::SpanExporter::builder()
-        .with_tonic()
-        .build()
-        .unwrap();
-
-    SdkTracerProvider::builder()
-        .with_batch_exporter(exporter)
-        .with_resource(Resource::builder().with_service_name("autha").build())
-        .build()
-}
-
-use axum::body::Bytes;
-use tower::ServiceBuilder;
-use tower_http::LatencyUnit;
 
 /// Create router.
 pub fn app(state: AppState) -> Router {
@@ -115,8 +48,6 @@ pub fn app(state: AppState) -> Router {
                 .make_span_with(DefaultMakeSpan::new().include_headers(true))
                 .on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
         )
-        // Add OLTP tracing.
-        .layer(OTelLayer)
         // Set a timeout.
         .layer(TimeoutLayer::new(Duration::from_secs(10)))
         // Add CORS preflight support.
@@ -137,23 +68,27 @@ pub fn app(state: AppState) -> Router {
         // `GET /users/:ID` goes to `get`.
         .route("/users/{user_id}", get(router::user::get))
         .with_state(state.clone())
-        .nest("/.well-known", well_known(state))
-        .route_layer(middleware::from_fn(metrics::track_metrics))
+        .nest("/.well-known", well_known::well_known(state))
+        .route_layer(middleware::from_fn(telemetry::track))
         .layer(middleware)
 }
 
 #[tokio::main]
 #[tracing::instrument]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tracer_provider = tracer();
+    // Initialize telemetry.
+    // initialize tracing.
+    let tracer_provider = telemetry::setup_tracer()?;
     global::set_tracer_provider(tracer_provider.clone());
 
-    // initialize tracing.
-    let (layer, task) = tracing_loki::builder()
-        .label("host", "autha")?
-        .extra_field("pid", process::id().to_string())?
-        .build_url(url::Url::parse("http://loki:3100")?)?;
-
+    // initialize logging.
+    let filter = EnvFilter::new("info")
+        .add_directive("hyper=off".parse().unwrap())
+        .add_directive("opentelemetry=off".parse().unwrap())
+        .add_directive("tonic=off".parse().unwrap())
+        .add_directive("h2=off".parse().unwrap())
+        .add_directive("reqwest=off".parse().unwrap());
+    let layer = telemetry::setup_logging().with_filter(filter);
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
@@ -168,10 +103,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    tokio::spawn(task); // init Loki delivrerer.
-
-    // init all telemetry logic.
-    let recorder_handle = metrics::setup_metrics_recorder()?;
+    // initialize metrics.
+    let recorder_handle = telemetry::setup_metrics_recorder()?;
 
     // load configuration and let it on memory.
     // init databases connection.
