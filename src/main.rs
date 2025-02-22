ed87d3a47d161f9ce86522a9fd92e93d4849787d
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-02-22 15:19:37 +0100

    Improve telemetry, add tracing (#522)
    * Add opentelemetry
    
    * Example of compose
    
    * Add otel configuration example
    
    * Support tracing on telemetry
    
    - Rename file from `metrics` to `telemetry`.
    - Add new functions related to tracing.
    
    * Add new crates
    
    Add OLTP crates.
    * Export OLTP logs
    
    * Add Loki configuration example

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,32 @@
 #[deny(missing_docs, unused_mut)]
 mod crypto;
 mod database;
-mod metrics;
 mod router;
 mod status;
+mod telemetry;
 mod user;
 mod well_known;
 
+use axum::body::Bytes;
 use axum::{
     http::{header, Method},
     middleware,
     routing::{get, post},
     Router,
 };
+use opentelemetry::global;
+use tower::ServiceBuilder;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::timeout::RequestBodyTimeoutLayer;
+use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::TraceLayer;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse};
+use tower_http::LatencyUnit;
+use tracing_subscriber::{prelude::*, EnvFilter};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use well_known::well_known;
 
 use std::env;
 use std::future::ready;
-use std::process;
 use std::time::Duration;
 
 #[derive(Clone)]
@@ -33,6 +38,26 @@ pub struct AppState {
 
 /// Create router.
 pub fn app(state: AppState) -> Router {
+    let middleware = ServiceBuilder::new()
+        // Add high level tracing/logging to all requests.
+        .layer(
+            TraceLayer::new_for_http()
+                .on_body_chunk(|chunk: &Bytes, latency: Duration, _span: &tracing::Span| {
+                    tracing::trace!(size_bytes = chunk.len(), latency = ?latency, "sending body chunk")
+                })
+                .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                .on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
+        )
+        // Set a timeout.
+        .layer(TimeoutLayer::new(Duration::from_secs(10)))
+        // Add CORS preflight support.
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_methods([Method::GET, Method::POST, Method::OPTIONS, Method::PATCH])
+                .vary([header::AUTHORIZATION]),
+        );
+
     Router::new()
         // `GET /status.json` goes to `status`.
         .route("/status.json", get(router::status::status))
@@ -40,28 +65,30 @@ pub fn app(state: AppState) -> Router {
         .route("/login", post(router::login::login))
         // `POST /create` goes to `create`.
         .route("/create", post(router::create::create))
-        // `GET /users/:USER_ID` goes to `get`.
+        // `GET /users/:ID` goes to `get`.
         .route("/users/{user_id}", get(router::user::get))
         .with_state(state.clone())
-        .nest("/.well-known", well_known(state))
-        .layer(TraceLayer::new_for_http())
-        .route_layer(middleware::from_fn(metrics::track_metrics))
-        .route_layer(
-            CorsLayer::new()
-                .allow_origin(Any)
-                .allow_methods([Method::GET, Method::POST, Method::OPTIONS, Method::PATCH])
-                .vary([header::AUTHORIZATION]),
-        )
+        .nest("/.well-known", well_known::well_known(state))
+        .route_layer(middleware::from_fn(telemetry::track))
+        .layer(middleware)
 }
 
 #[tokio::main]
+#[tracing::instrument]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize telemetry.
     // initialize tracing.
-    let (layer, task) = tracing_loki::builder()
-        .label("host", "autha")?
-        .extra_field("pid", process::id().to_string())?
-        .build_url(url::Url::parse("http://loki:3100")?)?;
+    let tracer_provider = telemetry::setup_tracer()?;
+    global::set_tracer_provider(tracer_provider.clone());
 
+    // initialize logging.
+    let filter = EnvFilter::new("info")
+        .add_directive("hyper=off".parse().unwrap())
+        .add_directive("opentelemetry=off".parse().unwrap())
+        .add_directive("tonic=off".parse().unwrap())
+        .add_directive("h2=off".parse().unwrap())
+        .add_directive("reqwest=off".parse().unwrap());
+    let layer = telemetry::setup_logging()?.with_filter(filter);
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
@@ -76,10 +103,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    tokio::spawn(task); // init Loki delivrerer.
-
-    // init all telemetry logic.
-    let recorder_handle = metrics::setup_metrics_recorder()?;
+    // initialize metrics.
+    let recorder_handle = telemetry::setup_metrics_recorder()?;
 
     // load configuration and let it on memory.
     // init databases connection.
@@ -104,6 +129,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await?;
     tracing::info!("listening on {}", listener.local_addr()?);
     axum::serve(listener, app).await?;
+
+    tracer_provider.shutdown().unwrap();
 
     Ok(())
 }
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,13 +1,41 @@
+//! Telemetry logic.
+//! Support tracing, metrics and logging.
 use axum::{
     extract::{MatchedPath, Request},
+    http::Version,
     middleware::Next,
     response::IntoResponse,
 };
 use metrics::{gauge, Unit};
 use metrics_exporter_prometheus::{BuildError, Matcher, PrometheusBuilder, PrometheusHandle};
-use std::time::{Duration, Instant};
+use opentelemetry::global;
+use opentelemetry::trace::{Span, TraceError, Tracer};
+use opentelemetry::KeyValue;
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::LogExporter;
+use opentelemetry_sdk::{logs::{LogError, SdkLogger}, trace::SdkTracerProvider};
+use opentelemetry_sdk::logs::SdkLoggerProvider;
+use opentelemetry_sdk::Resource;
 use sysinfo::{Pid, System};
 use tokio::time::sleep;
+
+use std::time::{Duration, Instant};
+
+fn ressources() -> Resource {
+    Resource::builder().with_service_name("autha").build()
+}
+
+/// Create tracer for OLTP.
+pub fn setup_tracer() -> Result<SdkTracerProvider, TraceError> {
+    let exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .build()?;
+
+    Ok(SdkTracerProvider::builder()
+        .with_batch_exporter(exporter)
+        .with_resource(ressources())
+        .build())
+}
 
 /// Create recorder for Prometheus metrics.
 pub fn setup_metrics_recorder() -> Result<PrometheusHandle, BuildError> {
@@ -29,6 +57,8 @@ pub fn setup_metrics_recorder() -> Result<PrometheusHandle, BuildError> {
     let mut system = System::new_all();
     let pid = Pid::from_u32(std::process::id());
 
+    // Create a loop to update system information.
+    // Wait 5 seconds before update it.
     tokio::spawn(async move {
         loop {
             system.refresh_all();
@@ -55,8 +85,25 @@ pub fn setup_metrics_recorder() -> Result<PrometheusHandle, BuildError> {
         .install_recorder()
 }
 
+/// Create OLTP exporter for logs.
+pub fn setup_logging() -> Result<OpenTelemetryTracingBridge<SdkLoggerProvider, SdkLogger>, LogError>  {
+    let exporter = LogExporter::builder()
+        .with_tonic()
+        .build()?;
+    let provider: SdkLoggerProvider = SdkLoggerProvider::builder()
+        .with_resource(ressources())
+        .with_batch_exporter(exporter)
+        .build();
+    Ok(opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge::new(&provider))
+}
+
 /// Track every metrics into one function. Cool.
-pub async fn track_metrics(req: Request, next: Next) -> impl IntoResponse {
+pub async fn track(req: Request, next: Next) -> impl IntoResponse {
+    // Init tracer.
+    let tracer = global::tracer("tracing-http");
+    let mut otel_span = tracer.start("http-request");
+
+    // Init all metrics data.
     let start = Instant::now();
     let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
         matched_path.as_str().to_owned()
@@ -64,20 +111,35 @@ pub async fn track_metrics(req: Request, next: Next) -> impl IntoResponse {
         req.uri().path().to_owned()
     };
     let method = req.method().clone();
+    let version = match req.version() {
+        Version::HTTP_09 => "HTTP/0.9", // should never appear!
+        Version::HTTP_10 => "HTTP/1.0",
+        Version::HTTP_11 => "HTTP/1.1",
+        Version::HTTP_2 => "HTTP/2",
+        Version::HTTP_3 => "HTTP/3",
+        _ => "UNKNOWN",
+    };
 
     let response = next.run(req).await;
 
     let latency = start.elapsed().as_secs_f64();
     let status = response.status().as_u16().to_string();
 
+    otel_span.set_attribute(KeyValue::new("version", version.to_owned()));
+    otel_span.set_attribute(KeyValue::new("path", path.clone()));
+    otel_span.set_attribute(KeyValue::new("method", method.to_string()));
+    otel_span.set_attribute(KeyValue::new("status", status.to_owned()));
+
+    // Metrics.
     let labels = [
         ("method", method.to_string()),
         ("path", path),
         ("status", status),
     ];
-
     metrics::counter!("http_requests_total", &labels).increment(1);
     metrics::histogram!("http_requests_duration_seconds", &labels).record(latency);
+
+    otel_span.end();
 
     response
 }
