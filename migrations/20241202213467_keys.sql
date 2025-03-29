aab695944bc95ef146206fe348d5e604a4e4af65
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-03-29 12:25:28 +0100

    Prefer `UNIQUE` constraint

--- migrations/20241202213467_keys.sql
+++ migrations/20241202213467_keys.sql
@@ -3,8 +3,6 @@
 CREATE TABLE IF NOT EXISTS keys (
   id            SERIAL  PRIMARY KEY,
   user_id       TEXT    REFERENCES users(id) ON DELETE CASCADE,
-  key           TEXT    NOT NULL,
+  key           TEXT    NOT NULL UNIQUE,
   created_at    DATE    NOT NULL DEFAULT NOW()
 );
-
-CREATE INDEX key_idx ON keys (key) INCLUDE (id);
