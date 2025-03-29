524b693af082dfec981d09a2d5333113e71ed631
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-03-29 17:49:19 +0100

    Check invite code

--- src/router/create.rs
+++ src/router/create.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use argon2::{
     password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, SaltString},
     Argon2, Params, Version,
@@ -5,10 +7,11 @@ use argon2::{
 use axum::{extract::State, http::StatusCode, Json};
 use regex_lite::Regex;
 use serde::{Deserialize, Serialize};
-use validator::{Validate, ValidationError};
+use validator::{Validate, ValidationError, ValidationErrors};
 
 use super::{ServerError, Valid};
-use crate::{database::Database, user::User};
+use crate::user::User;
+use crate::AppState;
 
 fn validate_id(vanity: &str) -> Result<(), ValidationError> {
     if !Regex::new(r"[A-Za-z0-9\-\.\_\~\!\$\&\'\(\)\*\+\,\;\=](?:[A-Za-z0-9\-\.\_\~\!\$\&\'\(\)\*\+\,\;\=]|(?:%[0-9A-Fa-f]{2}))$")
@@ -29,8 +32,14 @@ pub struct Body {
     id: String,
     #[validate(email(message = "Email must be formated."))]
     email: String,
-    #[validate(length(min = 8, message = "Password must contain at least 8 characters."))]
+    #[validate(length(
+        min = 8,
+        max = 255,
+        message = "Password must contain at least 8 characters."
+    ))]
     password: String,
+    #[validate(length(min = 4, max = 14, message = "Invalid invite code."))]
+    invite: Option<String>,
     _captcha: Option<String>,
 }
 
@@ -40,12 +49,41 @@ pub struct Response {
     token: String,
 }
 
+fn invalid_code() -> ValidationErrors {
+    let mut errors = ValidationErrors::new();
+    errors.add(
+        "invite",
+        ValidationError::new("invite").with_message("Invalid invite code.".into()),
+    );
+    errors
+}
+
 pub async fn create(
-    State(db): State<Database>,
+    State(state): State<AppState>,
     Valid(body): Valid<Body>,
 ) -> Result<(StatusCode, Json<Response>), ServerError> {
-    let email = crate::crypto::email_encryption(body.email);
+    // If invite is required, check it.
+    if state.config.invite_only {
+        if let Some(ref invite) = body.invite {
+            let is_used = sqlx::query!(
+                r#"SELECT used_at IS NOT NULL AS is_used FROM "invite_codes" WHERE code = $1"#,
+                invite
+            )
+            .fetch_one(&state.db.postgres)
+            .await
+            .map_err(|_| invalid_code())?
+            .is_used;
 
+            if is_used.unwrap_or(false) {
+                return Err(ServerError::Validation(invalid_code()));
+            }
+        } else {
+            return Err(ServerError::Validation(invalid_code()));
+        }
+    }
+
+    // Creation logic.
+    let email = crate::crypto::email_encryption(body.email);
     let password = {
         let salt = SaltString::generate(&mut OsRng);
         let params = Params::new(Params::DEFAULT_M_COST * 4, 6, Params::DEFAULT_P_COST, None)
@@ -63,20 +101,31 @@ pub async fn create(
     };
 
     sqlx::query!(
-        r#"INSERT INTO "users" (id, username, email, password) values ($1, $2, $3, $4)"#,
+        r#"INSERT INTO "users" (id, username, email, password) VALUES ($1, $2, $3, $4)"#,
         body.id.to_lowercase(),
         body.id,
         email,
         password
     )
-    .execute(&db.postgres)
+    .execute(&state.db.postgres)
     .await?;
 
     let user = User::default()
         .with_id(body.id.to_lowercase())
-        .get(&db.postgres)
+        .get(&state.db.postgres)
         .await?;
-    let token = user.generate_token(&db.postgres).await?;
+    let token = user.generate_token(&state.db.postgres).await?;
+
+    // Set invite code as used.
+    if state.config.invite_only {
+        sqlx::query!(
+            r#"UPDATE "invite_codes" SET used_by = $1, used_at = NOW() WHERE code = $2"#,
+            user.id,
+            body.invite.unwrap_or_default(),
+        )
+        .execute(&state.db.postgres)
+        .await?;
+    }
 
     Ok((StatusCode::CREATED, Json(Response { user, token })))
 }
@@ -96,16 +145,17 @@ mod tests {
     #[sqlx::test]
     async fn test_create_handler(pool: Pool<Postgres>) {
         let state = AppState {
-            db: Database { postgres: pool },
+            db: database::Database { postgres: pool },
             config: status::Configuration::default(),
         };
         let app = app(state);
 
-        let body = Body {
+        let body = router::create::Body {
             id: "user".into(),
             email: "test@gravitalia.com".into(),
             password: "Password1234".into(),
             _captcha: None,
+            invite: None,
         };
         let body = serde_json::to_string(&body).unwrap();
         let response = app
