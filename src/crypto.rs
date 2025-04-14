851f0ca9cc5a3655871a8d4ab9feaea69a82694d
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-04-14 20:52:37 +0200

    Support ECDSA via p256

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,6 +1,9 @@
 //! Cryptogragic logic.
 use fpe::ff1::{FlexibleNumeralString, Operations, FF1};
-use rsa::{RsaPublicKey, pkcs1::DecodeRsaPublicKey, pkcs8::DecodePublicKey};
+use p256::ecdsa::VerifyingKey;
+use p256::pkcs8::DecodePublicKey;
+use rsa::pkcs1::DecodeRsaPublicKey;
+use rsa::RsaPublicKey;
 
 const RADIX: u32 = 256;
 
@@ -23,7 +26,7 @@ pub fn email_encryption(data: String) -> String {
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum RsaError {
+pub enum KeyError {
     #[error(transparent)]
     Pkcs1(#[from] rsa::pkcs1::Error),
     #[error(transparent)]
@@ -33,14 +36,72 @@ pub enum RsaError {
 }
 
 /// Check if a key is well-formatted.
-pub fn check_key(key: &str) -> Result<RsaPublicKey, RsaError> {
+pub fn check_key(key: &str) -> Result<(), KeyError> {
     if key.contains("BEGIN RSA PUBLIC KEY") {
-        // Means it is PKCS-1.
-        Ok(RsaPublicKey::from_pkcs1_pem(key).map_err(RsaError::Pkcs1)?)
+        // Means it is PKCS#1 and only RSA.
+        RsaPublicKey::from_pkcs1_pem(key).map_err(KeyError::Pkcs1)?;
     } else if key.contains("BEGIN PUBLIC KEY") {
-        // Means it is PKCS-8.
-        Ok(RsaPublicKey::from_public_key_pem(key).map_err(RsaError::Pkcs8)?)
+        // Means it is PKCS#8 and could be even RSA or ECDSA.
+        if key.len() > 200 {
+            RsaPublicKey::from_public_key_pem(key).map_err(KeyError::Pkcs8)?;
+        } else {
+            VerifyingKey::from_public_key_pem(key).map_err(KeyError::Pkcs8)?;
+        }
     } else {
-        Err(RsaError::UnknownFormat)
+        return Err(KeyError::UnknownFormat);
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_email_encryption() {
+        // There is no env.
+        const RESULT: &str = "test@gravitalia.com";
+
+        let email = "test@gravitalia.com".to_string();
+        assert_eq!(&email_encryption(email), RESULT);
+    }
+
+    #[test]
+    fn test_rsa() {
+        // There is no env.
+        const REAL_KEY: &str = r#"-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH/cOrXe5GfnKdiFjP4T4g+WyKHE
+9WroOd0d6zbfsGuhEAYEmoSXV9HW1/HbLObzRb/O5yBC8Cb/sEyCGhgkSDelpQev
+sdhTB1QMqUXLcft1ehq0+4ZGrx0czfg3TYg60FX2nbtJOz4eQHa8kIif8NJsSpDW
+x7tMF74uf+o+1cNdAgMBAAE=
+-----END PUBLIC KEY-----"#;
+
+        assert!(check_key(REAL_KEY).is_ok());
+
+        const FAKE_KEY: &str = r#"-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCFhLBHV3S7h7DBTaBqPPg3Vrsy
+dvnqaT8jLUzgfOKxpizKYC1dtQx2JETAIMEJULIEN+OtA8/OU_PASDhaLnzkb6crTNojIokSXPWR
+7+VbBcsNVMhl3QneN1hpgRwlSAit8LfsRGFuuvj5Zb9cNMKPyFekXA0vPjH2OZMV
+GzdTXR1DHFS8P/saJQIDAQAB
+-----END PUBLIC KEY-----"#;
+        assert!(check_key(FAKE_KEY).is_err());
+    }
+
+    #[test]
+    fn test_ecdsa() {
+        // There is no env.
+        const REAL_KEY: &str = r#"-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAElInmvVOpx1FjEDIH6dGC6CxbtBjx
+02gUk8op1zxJvuA1zL0Pe07KO6wIjr+Ndi6HteDfqsC4Kzg+xDrUdTwRtw==
+-----END PUBLIC KEY-----"#;
+
+        assert!(check_key(REAL_KEY).is_ok());
+
+        const FAKE_KEY: &str = r#"-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEMcCSmtPOJLBrFImsV59akn3pmwGuebiT
+pQkthCHdjBbLyMZDI//d7+I3AxnZ+/QyFO32e8tvkYdAT4MM2jb0AyxA
+-----END PUBLIC KEY-----"#;
+        assert!(check_key(FAKE_KEY).is_err());
     }
 }
