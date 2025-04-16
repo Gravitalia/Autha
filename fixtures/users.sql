1fd85672542bb159c3780fa8db089330e70ca4ed
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-04-16 20:47:30 +0200

    Add fixiture

---
+++ fixtures/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, username, email, password)
+    VALUES (
+        'admin', 'Administrator', 'admin@gravitalia.com',
+        '$argon2id$v=19$m=77824,t=6,p=1$Ug0fGROyFvpRxgU93lBUIw$2wgPgNhWY2Pd3AUsBh2NPytvEQoE0zIiYexVW/V0qtw'
+    );
