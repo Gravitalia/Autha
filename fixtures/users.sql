d59efb346d2c166a9d1329f9b57cca38e1648444
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-05-19 11:53:13 +0200

    update email encryption

--- fixtures/users.sql
+++ fixtures/users.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (id, username, email, password)
     VALUES (
-        'admin', 'Administrator', '26378b206d60de2ed8819fc1bc9502ee2abcd7e1',
+        'admin', 'Administrator', '3601ff3a929d30b044c7ec7722c0d5da0fcba9acca82ded8b781e999b01aa33a',
         '$argon2id$v=19$m=77824,t=6,p=1$Ug0fGROyFvpRxgU93lBUIw$2wgPgNhWY2Pd3AUsBh2NPytvEQoE0zIiYexVW/V0qtw'
     );
