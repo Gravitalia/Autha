e0d82edecc8c3d8ffc0a97a0757bc1dcd0bafa9a
RealHinome <acn@gravitalia.com>
2025-11-26 13:52:35 +0100

    Update email hash for fixture

--- fixtures/users.sql
+++ fixtures/users.sql
@@ -1,6 +1,5 @@
 INSERT INTO users (id, username, email_hash, email_cipher, password)
     VALUES (
-        'admin', 'Administrator', '1dd74608b833747c11ea9a0831382811cb28f4cd4d3d90d04121191a1052ddb3',
-        '1adbd651bd63fd7825a440c2e04d2447fed0aebddc0479cb2468c98fc55d8a17e38d8539123965e2f14a4407b56e8f',
+        'admin', 'Administrator', 'b58e1c8acc2ef7d04325a52c71e25ac54aea27809f0e5d8c9665415200807a38', '',
         '$argon2id$v=19$m=77824,t=6,p=1$Ug0fGROyFvpRxgU93lBUIw$2wgPgNhWY2Pd3AUsBh2NPytvEQoE0zIiYexVW/V0qtw'
     );
