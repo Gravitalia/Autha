160fe07faa83a0b931f47809cb0959f9e2792999
RealHinome <acn@gravitalia.com>
2026-03-10 16:20:59 +0100

    Move migrations folder

--- crates/adapters/migrations/20241202213467_keys.sql
+++ crates/adapters/migrations/20241202213467_keys.sql
@@ -3,6 +3,9 @@
 CREATE TABLE IF NOT EXISTS keys (
   id            SERIAL  PRIMARY KEY,
   user_id       TEXT    REFERENCES users(id) ON DELETE CASCADE,
-  key           TEXT    NOT NULL UNIQUE,
+  device_name   TEXT    NOT NULL DEFAULT 'unknown',
+  pem           TEXT    NOT NULL UNIQUE,
   created_at    DATE    NOT NULL DEFAULT NOW()
 );
+
+CREATE INDEX idx_keys_user_id ON keys(user_id);
